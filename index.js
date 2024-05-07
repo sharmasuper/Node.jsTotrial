686a677322cee3d01bec86670729a7fbd7e1bde2
sharmasuper <mohit6375sharma@gmail.com>
2024-05-07 11:59:06 +0530

    lecture - 8 all about Package.json file

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-const http = require('http');
+const colors = require('colors')
 
-function datacontrol(req,resp){
-    resp.write("<h1>Hello this is anil Mohit<h1>");
-    resp.end();
-}
-
-// http.createServer((req,resp)=>{     //asai bhi kar sktai h
- 
-//     // resp.write("<h1>Hello this is anil sidhu<h1>");
-//     // resp.end();
- 
-// }).listen(4500)   
-http.createServer(datacontrol).listen(4500)
-
-
-//createServer take function as parameter
-
-// test(a,b){
-
-// }
-// test()
+console.log("hello mohit".red)
