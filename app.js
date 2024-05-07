bf91a4494c26a34cefc5499943633b1da00d72cd
sharmasuper <mohit6375sharma@gmail.com>
2024-05-07 10:29:22 +0530

    export and import not support

---
+++ app.js
@@ -0,0 +1,2 @@
+export let x = 10;
+export let y = 20;
---
+++ index.js
@@ -0,0 +1,6 @@
+//node js does not support export an and import 
+
+import {x} from './app'
+console.log(x)
+//noe js does not support export and import
+
