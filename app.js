2d84964e176df774c0ea9df2edd9374ed3c6a456
sharmasuper <mohit6375sharma@gmail.com>
2024-05-07 10:56:32 +0530

    export with module use

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-export let x = 10;
-export let y = 20;
+//we use module as import and export 
+module.exports = {
+    x:10,
+    y:20,
+    z : function(){
+        return 10;
+    }
+}
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-//node js does not support export an and import 
+const app = require('./app')
 
-import {x} from './app'
-console.log(x)
-//noe js does not support export and import
+// console.log(app.x)
+// console.log(app.y)
+// console.log(app)
+// console.log(app.z)
+// console.log(app.z())
+//use filter method
+arr = [2,3,4,5,6,7]
 
+const result  = arr.filter((item)=>{
+    return item === 3
+}) 
+
+console.log(result)
