30393c57ade65601becaf76c6e7ec42684354c60
sharmasuper <mohit6375sharma@gmail.com>
2024-05-08 20:19:12 +0530

    Request and Response Params in get method use lecture 19

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const express = require('express')
 const app = express()
 
-app.get('/',(req,res)=>{
-  res.send("hello , this is home page")
-
+app.get('/',(req,resp)=>{
+    //console.log("data send by browser ,", req.query)   //result check on terminal - {nam:"anil",sharma:"ram"}
+    console.log("data send by browser ,", req.query.sharma)
+//    resp.send("welcome ,this is home page") 
+  resp.send('welcome,this is home page  '+req.query.sharma)
 })
-app.get('/about',(req,res)=>{
-    res.send("hello , this is about page")
-    
-  })
 
+app.get('/about',(req,resp)=>{
+    resp.send("welcome,this is about page")
+})
 
-  app.get('/help',(req,res)=>{
-    res.send("hello , this is help page")
-    
-  }) 
-
-app.listen(5000)  //server per run karatai h 
-
+app.get("/help",(req,resp)=>{
+    resp.send("welcome this is help page")
+})
+                        //http://localhost:5000/?name=anil&sharma=ram asai get api bnai
+app.listen(5000)
 
 
 
