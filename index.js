d4d506b8c1ea564a841cf8057a1d3d4143cb055a
sharmasuper <mohit6375sharma@gmail.com>
2024-05-08 12:53:30 +0530

    input form command line

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
+console.log(process)
+//process ek object hota h jskai ander node js ka almost sab hota
+//property ko access karo
+//console.log(process.argv)
+ //argv means = argunment vector
+//he give two response frist
+//frist jaha hamari file save /install h vo
+//second jaha sai hum isko run kar rahai vo ya file run kar rahai h
+//iskai badh jo bhi hum input likhtai vo hami mil jata h
+//ex - node .\index.js hello
+//result frist - install, second - file run path thrid- argunment ----
 
-const http = require('http')
-const data = require('./data')
-http.createServer((req,resp)=>{         
-    resp.writeHead(200,{'Content-Type':'application\json'})        //abh hum response send kar rahai h rep.writehead(reponsecode,jistypeka data send kartai h)
-                                   //jab user sai data lagai tab req ka use karraugai              
-  // resp.write(JSON.stringify({name:"Mohit sharma",email:'Mohit@gmail.com'}))  //body
-  resp.write(JSON.stringify(data))
-    resp.end()
-}).listen(4500) 
+//target - 
+//console.log(process.argv[2])
+//command run - node .\index.js hello mohit
+//result - hello
 
 
+//ab iski help sai meanfull chijai banatai h
+const fs = require('fs')
+const input  = process.argv
+ //fs.writeFileSync(input[3],input[4])
+//writeFilesync create file fristargunment - fileName
+//                        second argunment - jobhi file kai ander data ko add karna chahtai h
+//command run - node index.js apple.text 'this file make process command"
+//condiiton base
+if(input[2] == 'add'){
+    fs.writeFileSync(input[3],input[4])
+}else if(input[2] == 'remove'){
+    fs.unlinkSync(input[3])
+}else{
+    console.log("invalid input")
+}
+
+
+node index.js apple.text 'this file make process command"  run command
+node index.js remove orane isssai remove ho jayga
+node index.js add orane.text "file add text"
 
 
 
