1358dec3cd537d601df4a0cba00dfd37bbdd976e
sharmasuper <mohit6375sharma@gmail.com>
2024-05-08 13:34:20 +0530

    display fils list form folder and lecture -13

--- index.js
+++ index.js
@@ -1,41 +1,28 @@
-console.log(process)
-//process ek object hota h jskai ander node js ka almost sab hota
-//property ko access karo
-//console.log(process.argv)
- //argv means = argunment vector
-//he give two response frist
-//frist jaha hamari file save /install h vo
-//second jaha sai hum isko run kar rahai vo ya file run kar rahai h
-//iskai badh jo bhi hum input likhtai vo hami mil jata h
-//ex - node .\index.js hello
-//result frist - install, second - file run path thrid- argunment ----
-
-//target - 
-//console.log(process.argv[2])
-//command run - node .\index.js hello mohit
-//result - hello
-
-
-//ab iski help sai meanfull chijai banatai h
 const fs = require('fs')
-const input  = process.argv
- //fs.writeFileSync(input[3],input[4])
-//writeFilesync create file fristargunment - fileName
-//                        second argunment - jobhi file kai ander data ko add karna chahtai h
-//command run - node index.js apple.text 'this file make process command"
-//condiiton base
-if(input[2] == 'add'){
-    fs.writeFileSync(input[3],input[4])
-}else if(input[2] == 'remove'){
-    fs.unlinkSync(input[3])
-}else{
-    console.log("invalid input")
-}
+// // fs.unlinkSync('apple.text') file delete command
+// fs.writeFileSync('apple.txt','this is a apple file')
 
+//issai sirf ek file create ho rahi h but mai chahtha hu ki loop mai ek sath kahi file cereate ho tab
+//jab bhi file create karai to humai uska path ki need hoti ki hum issai kis folder mai create karaigai 
+// const fs = require('fs')
+const path = require('path')
+const dirPath = path.join(__dirname,'files');
+// console.log(dirPath)
+// for(let i=0;i<5;i++){
+//     fs.writeFileSync(dirPath+"/hello"+i+".txt","a simple tet file")
+// }
+//ab is file ko read karna h 
+//read tab kara sktai h jab file kai ander content ho
+ fs.readdir(dirPath,(error,files)=>{
+    // console.warn(files)   //return files in array
+    files.forEach((item)=>{
+        console.log("files name is ",item)
+    })
+ })
 
-node index.js apple.text 'this file make process command"  run command
-node index.js remove orane isssai remove ho jayga
-node index.js add orane.text "file add text"
+//hum iss flder ka data dusrai folder mai access nahi kar sktai 
+//nahi get because yai ek web server
 
+console.log("notes")
 
 
