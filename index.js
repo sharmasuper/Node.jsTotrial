50005c2555d7001e61bee7e7112a9a47e38f59c8
sharmasuper <mohit6375sharma@gmail.com>
2024-05-08 16:41:14 +0530

    Asynchronous Programming Language

--- index.js
+++ index.js
@@ -1,49 +1,19 @@
-const fs = require('fs')
-const path = require('path')
-const dirpath = path.join(__dirname,"crud")
-//fs.writeFileSync('apple.txt',"this file text")  //yai file merai root folder mai banaigi
-//isliy mai issai crud folder mai file banauga
-//create method
-const filepath =  `${dirpath}/apple.txt`
-// fs.writeFileSync(filepath,"this is simple text file")
-//read method
-// fs.readFile(filepath,'utf8',(error,item)=>{
-//   console.log(item)
-// })
-//update method
-// fs.appendFile(filepath," and file name is apple.txt",(err)=>{
-//     if(!err){
-//         console.log("file is updated")
-//     }
-// })
-
-//rename
-// fs.rename(filepath,`${dirpath}/fruit.txt`,(err)=>{
-//     if(!err) {console.log("file name isUpdate")}
-// })
-
-//Delete
-
-fs.unlinkSync(`${dirpath}/fruit.txt`)
+console.log("start exe ....")
 
 
 
+setTimeout(()=>{
+    console.log("Logic exc ----")
+},2000)
+console.log("complete exe ....")
+//dropback example in acncronous programming
+let a = 2
+let b = 5
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+setTimeout(()=>{
+    b=20;
+},2000)
+console.log(a+b)
 
 
 
