112cd347d56f0c3b93a0c3180eaeaa0e3b7b9f66
sharmasuper <mohit6375sharma@gmail.com>
2024-05-08 10:58:40 +0530

    Simple and basic api

---
+++ data.js
@@ -0,0 +1,9 @@
+const data = [
+    {name:"Mohit sharma",email:'Mohit@gmail.com'},
+    {name:"Laddu Gopal sharma",email:'Laddu@gmail.com'},
+    {name:"Badham sharma",email:'Bhadam@gmail.com'},
+    {name:"sunita sharma",email:'Sunita@gmail.com'},
+    {name:"Vijaya jiji sharma",email:'Vijaya@gmail.com'},
+    {name:" Tannu",email:'Tannu@gmail.com'}
+]
+module.exports = data
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const colors = require('colors')
 
-console.log("hello mohit".red)
-
+const http = require('http')
+const data = require('./data')
+http.createServer((req,resp)=>{         
+    resp.writeHead(200,{'Content-Type':'application\json'})        //abh hum response send kar rahai h rep.writehead(reponsecode,jistypeka data send kartai h)
+                                   //jab user sai data lagai tab req ka use karraugai              
+  // resp.write(JSON.stringify({name:"Mohit sharma",email:'Mohit@gmail.com'}))  //body
+  resp.write(JSON.stringify(data))
+    resp.end()
+}).listen(4500) 
 
 
 
