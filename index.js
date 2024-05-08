088085a1b6843f0b181034bc61704c27ac98e5b8
sharmasuper <mohit6375sharma@gmail.com>
2024-05-08 19:38:35 +0530

    how node js work lecture -17

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-let a = 20
-let b = 0
-let waitingData  = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        resolve(30)
-    },2000)
-})
-waitingData.then((data)=>{
-    b = data
-    console.log(a+b)
-})
+//how Node js is work
+
+//code =>calll Stack => NodeAPI =>event Loop
+console.log("Starting up")
+
+setTimeout(()=>{
+    console.log("2 second log")
+},2000)
+
+setTimeout(()=>{
+    console.log("0 second log")
+},0)
+
+console.log("Finishing up")
