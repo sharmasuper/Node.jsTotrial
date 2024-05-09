df49afc8388664b0e274685ac35be7d77bdf0004
sharmasuper <mohit6375sharma@gmail.com>
2024-05-09 16:26:46 +0530

    lecture -21 Make Html page

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 
-app.get('/',(req,resp)=>{
-    //console.log("data send by browser ,", req.query)   //result check on terminal - {nam:"anil",sharma:"ram"}
-    console.log("data send by browser ,", req.query.sharma)
-//    resp.send("welcome ,this is home page") 
-  //resp.send('welcome,this is home page  '+req.query.sharma)
-  resp.send(`<h1>welcome , to home page</h1>,<a href="/about">Goabout</a>`)
 
-  resp.send()
+// const publicPath = path.join(__dirname,'public')
+const publicpath = path.join(__dirname,'public')  //yai ab public folder ki jitni bhi file h unko access kraiga
+ //node js root page ko by default index.html samjtha h
+  ///but isasai hum style.css or image add nahi kar sktai 
+                                   //isko esai access kar sktai h http://localhost:5000/about.html
+app.use(express.static(publicpath));
+
+app.listen(5000,()=>{
+  console.log("api hit")
 })
 
-app.get('/about',(req,resp)=>{
-    resp.send(`
-       <input type="text" placeholder="user name" value=${req.query.name} />
-       <button>ClickMe</button>
-    `)                          //html add 
-})
 
-app.get("/help",(req,resp)=>{
-   // resp.send({name:"anil",email:"anil@gmail.com"})  //send json data
-   resp.send([{name:"Mohit",email:"mohit@gmial.com"},{name:"sharma",email:"sharma@gmail.com"}])
-})
-                        //http://localhost:5000/?name=anil&sharma=ram asai get api bnai
-app.listen(5000)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
