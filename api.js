7769b4da463529931605afb0e2da4ce1ac3f5133
sharmasuper <mohit6375sharma@gmail.com>
2024-05-13 23:21:09 +0530

    basic Get api with mongodb lecture -35

---
+++ api.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const {dbConnect} = require('./mongodb') 
+const app = express();
+
+app.get('/',async(req,resp)=>{
+    let data = await dbConnect()
+    data = await data.find().toArray();
+    console.log(data)
+    //resp.send({name:"anil"})
+      resp.send(data)                //basic api with mongodb
+})
+
+app.listen(5000)
+
+
+
+
+
+
