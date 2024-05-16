71abd983e8ddda88c43d5c08c506dbc58c1fe9f2
sharmasuper <mohit6375sharma@gmail.com>
2024-05-17 01:33:26 +0530

    Lecture -39 Mongoose starting

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-const {dbConnect} = require('./mongodb')
+const mongoose = require('mongoose')
+const main = async() =>{
+    await mongoose.connect("mongodb://localhost:27017/e-come")
+    const ProductSchema = new mongoose.Schema({
+        name : String,
+        price : Number  // now price will be entering in result 
+    });
+    const ProductModal = mongoose.model('products',ProductSchema)
+  //products have database name
+  //let data = new  ProductModal({name:"m8"})   //result name enter ho gaya
+  let data = new ProductModal({name:"m8",price:1000}) //but Price have not enter bacause In validation we had not written price
+  let result = await data.save();
+  console.log(result)
 
-const read = async() =>{
-try{
- const add = await dbConnect() 
- const data = await add.find().toArray() 
- console.log(data)
-  } 
-  catch(error){
-    throw("some thing went wrong",error) 
-  }
-} 
+}
+main()
 
-read() 
+
+
+// {
+//     name: 'm8',
+//     price: 1000,
+//     _id: new ObjectId('664665f89d9e1e40447ff2b0'),
+//     __v: 0
+//   }
+// result
+
+
+
+
+
+
