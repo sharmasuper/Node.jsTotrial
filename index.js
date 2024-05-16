9ded1d3645855bbc7a383a45aa0f69f8d2e31081
sharmasuper <mohit6375sharma@gmail.com>
2024-05-17 00:27:16 +0530

    Check it

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const {dbConnect} = require('./mongodb')
 
-
 const read = async() =>{
-  try{
+try{
  const add = await dbConnect() 
  const data = await add.find().toArray() 
  console.log(data)
-  }
+  } 
   catch(error){
     throw("some thing went wrong",error) 
   }
-}
+} 
 
 read() 
