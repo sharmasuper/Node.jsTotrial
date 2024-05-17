09c77fe4dc7151d9056d45217e58df93d1fccc88
sharmasuper <mohit6375sharma@gmail.com>
2024-05-17 06:20:40 +0530

    Seach api With multiple field

--- index.js
+++ index.js
@@ -2,46 +2,20 @@ const express = require('express')
 require('./config')
 const Product = require('./Product')
 const app = express()
+
 app.use(express.json())
-app.post("/create",async (req,resp)=>{
-    let data = new Product(req.body)
-    let result = await data.save()
-    console.log(result)                       //these Post api have send only one object not array 
-                                                 //test api i thunderClient and Postman
-    // console.log(req.body)
-    resp.send("done")
+app.get("/search/:key",async(req,resp)=>{  //but hamai query ek hi parameter pass karna hoga 
+    console.log(req.params)               //  such as -  http://localhost:5000/search/Hotorola
+    let data = await Product.find(
+        {
+            "$or":[
+                {"category":{$regex:req.params.key}},   //if you search two parameter like as brand 
+                {"brand":{$regex:req.params.key}}
+            ]
+        }
+    )
+  resp.send(data) 
 
 })
-
-app.get('/list',async(req,resp)=>{
-    let data = await Product.find()
-    resp.send(data)
-})
-
-//delete
-app.delete('/delete/:_id',async(req,resp)=>{
-     console.log(req.params) 
-     let data = await Product.deleteOne(req.params) 
-     resp.send(data)
-})
-
-app.put('/update/:_id',async (req,resp)=>{
-    console.log(req.params)
-    console.log(req)
-    let data = await Product.updateMany(req.params,{$set:req.body})  //UpdateOne
-    resp.send(data)    //mai data ko query params sai bhi lai sktah hu or update ka sktaha hu or body sai bhi
-})
-
-
-
-
 
 app.listen(5000)
-
-
-
-
-
-
-
-
