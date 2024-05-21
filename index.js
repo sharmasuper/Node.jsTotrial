32564d582574766e14fbbac8c0d545a2b9b2d68d
sharmasuper <mohit6375sharma@gmail.com>
2024-05-21 13:32:39 +0530

    cookies in nodejs

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
 const express = require('express')
-require('./config')
-const Product = require('./Product')
+const cookieparser = require('cookie-parser')
 const app = express()
 
-app.use(express.json())
-app.get("/search/:key",async(req,resp)=>{  //but hamai query ek hi parameter pass karna hoga 
-    console.log(req.params)               //  such as -  http://localhost:5000/search/Hotorola
-    let data = await Product.find(
-        {
-            "$or":[
-                {"category":{$regex:req.params.key}},   //if you search two parameter like as brand 
-                {"brand":{$regex:req.params.key}}
-            ]
-        }
-    )
-  resp.send(data) 
+app.use(cookieparser())
 
+
+app.get("/",(req,resp)=>{
+    resp.send("hello Cookie")
 })
 
-app.listen(5000)
+app.get("/set-cookie",(req,resp)=>{
+    // resp.setHeader('set-cookie',"foo=bar")
+    //resp.setHeader("name of header","key=value")
+    resp.cookie('foo','bar',{
+        // maxAge:5000,
+        // expires:new Date('22 May 2024')
+        // httpOnly : true  //it means this cookie could not be accessable in client side  ; this not secure
+      //  secure : true // then the cookie only were set only http over connection not over the plane http connection 
+    //   domain : 'example.com'
+    })
+    // resp.cookie('fizz','buzz')
+    resp.send("cookie are set")
+})
+
+app.get('/get-cookie',(req,resp)=>{
+    console.log(req.cookies)
+    resp.send(req.cookies)
+})
+
+app.get('/del-cookie',(req,resp)=>{
+
+    resp.clearCookie('fizz')
+    resp.send("Cookie has been deleted")
+    
+})
+
+
+app.listen(3000,()=>{
+    console.log("Cookie server on port 3000")
+})
+
+
+
+
+
+
+
+
+
