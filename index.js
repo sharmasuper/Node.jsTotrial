bebc3d810391a7a4185d27b5740686523898232c
sharmasuper <mohit6375sharma@gmail.com>
2024-05-24 03:09:45 +0530

    router.param method hook use

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
-//goal basic base 
-//router.all topic
-const express = require("express")
-const router = express.Router()
+
+const express = require('express')
 const app = express()
-router.route('/example').get((req,resp)=>{
-    resp.send("hello my name is mohit sharma get")
-})
-router.route('/example').post((req,resp)=>{
-    resp.send("hello post")
-})
-router.route('/example').put((req,resp)=>{
-    resp.send("hello put")
-})
-router.route('/example').delete((req,resp)=>{
-      resp.send("hello delete")
-})
-router.route('*').all((req,resp)=>{
-    resp.status(404).send(`Request Method: ${req.method}, Request Path: ${req.path}`) 
+const router = express.Router()
+app.use(express.json())  
+
+router.param('id',(req,resp,next,id)=>{
+    console.log("recived id "+ id)
+   if(!Number.isInteger(Number(id))){
+    return resp.status(400).send("please fill correcct param")
+   }
+   req.id = Number(id)
+   next()
 })
 
-app.use('/add',router)
+router.param('name',(req,resp,next,name)=>{
+    console.log('you recieve name is '+name)
+    if(typeof name === 'string'&& name.length>=3){
+        req.name = name
+        next()
+    }else{
+        return resp.status(400).send("Invalid route name please fill correct route")
+    }
+})
+
+
+
+router.route('/item/:id').get((req,resp)=>{
+    resp.send(`hello get id is ${req.id}`)
+})
+
+router.route('/hello/:name').get((req,resp)=>{
+    resp.send("hello only take name mohit you see"+req.name)
+})
+
+router.route('*').all((req,resp)=>{
+    resp.send("hello 404 page")
+})
+app.use('/API',router)
+
 app.route('*').all((req,resp)=>{
-    resp.status(404).send("404page 1")
+    resp.status(404).send("404 page Page Not found")
 })
+
 app.listen(3000,()=>{
-    console.log('api hit successfully')
+    console.log("api hit successfully")
 })
+
 
 
