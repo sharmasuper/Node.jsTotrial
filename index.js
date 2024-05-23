f66ad9a1da12abe2eb21b449669bdfde4003d25e
sharmasuper <mohit6375sharma@gmail.com>
2024-05-24 04:00:25 +0530

    res.app methd example

--- index.js
+++ index.js
@@ -1,50 +1,30 @@
-
 const express = require('express')
 const app = express()
-const router = express.Router()
-app.use(express.json())  
 
-router.param('id',(req,resp,next,id)=>{
-    console.log("recived id "+ id)
-   if(!Number.isInteger(Number(id))){
-    return resp.status(400).send("please fill correcct param")
-   }
-   req.id = Number(id)
-   next()
-})
+app.use(express.json());
+app.set('appName','express res app example mohit')
 
-router.param('name',(req,resp,next,name)=>{
-    console.log('you recieve name is '+name)
-    if(typeof name === 'string'&& name.length>=3){
-        req.name = name
-        next()
-    }else{
-        return resp.status(400).send("Invalid route name please fill correct route")
-    }
-})
+app.use((req,resp,next)=>{
+    console.log(`or middleware response message is ${resp.app.get('appName')}`)
+    next()   
+}) 
 
-
-
-router.route('/item/:id').get((req,resp)=>{
-    resp.send(`hello get id is ${req.id}`)
-})
-
-router.route('/hello/:name').get((req,resp)=>{
-    resp.send("hello only take name mohit you see"+req.name)
-})
-
-router.route('*').all((req,resp)=>{
-    resp.send("hello 404 page")
-})
-app.use('/API',router)
-
-app.route('*').all((req,resp)=>{
-    resp.status(404).send("404 page Page Not found")
+app.route('/api').get((req,resp)=>{
+    resp.status(200).send(`hello my message is ${req.app.get('appName')}`)
 })
 
 app.listen(3000,()=>{
-    console.log("api hit successfully")
+
+    console.log("hello api hit successfully")
+
+
 })
+
+
+
+
+
+
 
 
 
