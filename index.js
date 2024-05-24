7680fbed0804284f9b3f0d55fbd5bf45bd93f348
sharmasuper <mohit6375sharma@gmail.com>
2024-05-24 13:14:21 +0530

    resp.attachment method

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path')
 
-app.route('/').get((req,resp)=>{
-    resp.append('multipl-Value',['val1','val2','val3'])
-   
-    resp.send("hello append")
-})
-app.listen(3000,()=>{
-    console.log("api hit successfully")
-})
+app.get('/download', (req, res) => {
+  // Set the filename for the attachment
+  const filename = path.join(__dirname,'History.txt');
+console.log(filename)
 
+  // Set the Content-Disposition header to attachment with the specified filename
+  res.attachment(filename);
 
+  // Send the content of the file
 
+  res.send("send attched file to the client");
+});
 
-
-
-
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
