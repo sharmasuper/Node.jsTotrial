dfa188a03c2a5de1f5379ed594127810461e7d05
sharmasuper <mohit6375sharma@gmail.com>
2024-05-24 13:18:47 +0530

    attachment file and imagehas send not blank

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const path = require('path')
 
 app.get('/download', (req, res) => {
-  // Set the filename for the attachment
-  const filename = path.join(__dirname,'History.txt');
-console.log(filename)
+  const imagePath = path.join(__dirname, 'History.txt'); //image bhi send kar sktai h image.jpg file write karkai
+  const filename = 'image.txt';
 
   // Set the Content-Disposition header to attachment with the specified filename
   res.attachment(filename);
 
-  // Send the content of the file
-
-  res.send("send attched file to the client");
+  // Send the image file
+  res.sendFile(imagePath, (err) => {
+    if (err) {
+      console.error('Error occurred while sending file:', err);
+      res.status(500).send('Error occurred while downloading the file.');
+    }
+  });
 });
 
 app.listen(3000, () => {
