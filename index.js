b4d9bcf0b0a00edde0ed24ddb280ed7a669dd2d1
sharmasuper <mohit6375sharma@gmail.com>
2024-05-25 19:01:17 +0530

    end() method

--- index.js
+++ index.js
@@ -1,50 +1,22 @@
-// const express = require('express')
-// const path = require('path')
-// const app = express()
-
-// app.get('/download',(req,resp)=>{
-//     const filepath = path.join(__dirname,'history.txt',)
-//     const filename = 'download_example.txt'
-//     resp.download(filepath,filename,(err)=>{
-//         if(err){ 
-//             console.error("error whiling download file",err)
-//             resp.status(500).send('Error ocured while file downloading')
-//         }else {
-//             console.log('file download successfully')
-           
-//         }
-//     }) 
-
-// })
-
-// app.listen(3000,()=>{
-//     console.log("server is running on port 3000")
-// })
-
 const express = require('express')
-const path = require('path')
-const filedir = path.join(__dirname,'History.txt')
 const app = express()
-app.get("/attachment",(req,resp)=>{
-      resp.attachment('hello.txt')
-      resp.sendFile(filedir)
+// app.use(express.json())
+
+app.route('/end').get((req,resp)=>{
+    console.log("hello how are you")
+    resp.write("response send successfully")
+    resp.end()
 })
+app.route('/hello').get((req,resp)=>{
+    // resp.write("hello route how are you")
+    resp.setHeader('Content-Type', 'application/json')
+    const data = JSON.stringify([{"name":"Ram","hello":"sharma"}])
+    // // resp.write(data)
+    resp.end(data) 
+})
+
 app.listen(3000,()=>{
-    console.log("api hiy successfully")
+    console.log("api hit successfully") 
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
