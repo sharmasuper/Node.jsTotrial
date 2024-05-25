0159e5ffbccbb52fea3483853fdf3e6a3e7780e8
sharmasuper <mohit6375sharma@gmail.com>
2024-05-25 18:20:00 +0530

    file download method and both difference and attachment

--- index.js
+++ index.js
@@ -1,41 +1,50 @@
 // const express = require('express')
+// const path = require('path')
 // const app = express()
 
-// app.get("/set-cookie",(req,resp)=>{
-//   resp.cookie('name','value',{
-//     maxAge :60*60*24*1000,
-//     httpOnly:true,
-//     secure :true
-//   })
-  
-//   resp.send("hello home route")
-// })
+// app.get('/download',(req,resp)=>{
+//     const filepath = path.join(__dirname,'history.txt',)
+//     const filename = 'download_example.txt'
+//     resp.download(filepath,filename,(err)=>{
+//         if(err){ 
+//             console.error("error whiling download file",err)
+//             resp.status(500).send('Error ocured while file downloading')
+//         }else {
+//             console.log('file download successfully')
+           
+//         }
+//     }) 
 
+// })
 
 // app.listen(3000,()=>{
-//   console.log("api hit successfully")
+//     console.log("server is running on port 3000")
 // })
 
-const express = require("express")
+const express = require('express')
+const path = require('path')
+const filedir = path.join(__dirname,'History.txt')
 const app = express()
-app.get("/cookie-set",(req,resp)=>{
-  resp.cookie('name','mohit',{
-    maxAge :2000,
-    httpOnly : true,
-    secure : true
-  })
-  resp.send("cookie is set successfully")
+app.get("/attachment",(req,resp)=>{
+      resp.attachment('hello.txt')
+      resp.sendFile(filedir)
 })
-
-app.get("/clear-cookie",(req,resp)=>{
-  resp.clearCookie('name',{
-    httpOnly:true,
-    secure :true
-  })
-  resp.send("cookie clear seccessfully")
-})
-
 app.listen(3000,()=>{
-  console.log("api send successfully")
+    console.log("api hiy successfully")
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
