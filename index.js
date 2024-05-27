31d83bbd4cce0ec2f07b22f464f158ba045515ec
sharmasuper <mohit6375sharma@gmail.com>
2024-05-27 11:31:09 +0530

    set method

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.sendStatus(200); // Sends a 200 OK status with the message "OK"
-});
-
-app.get('/not-found', (req, res) => {
-  res.sendStatus(404); // Sends a 404 Not Found status with the message "Not Found"
-});
-
-app.get('/error', (req, res) => {
-  res.sendStatus(500); // Sends a 500 Internal Server Error status with the message "Internal Server Error"
+  res.set({
+    'Content-Type': 'application/json', // Sets the Content-Type header to application/json
+    'X-Custom-Header': 'CustomHeaderValue' // Sets a custom header
+  });
+ // res.json({ message: 'Hello World' }); // Sends a JSON response
+ res.send({message:"hello world"})
 });
 
 app.listen(port, () => {
