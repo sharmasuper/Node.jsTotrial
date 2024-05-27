f441750dc103acd73729b7df5f564de39bb4fc45
sharmasuper <mohit6375sharma@gmail.com>
2024-05-27 07:22:54 +0530

    resp.render() method use case

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-// Certainly! The res.redirect() method in Express.js is used to redirect the client to a
-//  different URL. This method can take two arguments: the optional HTTP status code and the URL to redirect
-//  to. If the status code is not provided, it defaults to 302 Found.
-
 const express = require('express')
 const app = express()
-app.route("/hello").get((req,resp)=>{
-    resp.redirect('http://google.com')
-    //it is not allow to use resp.send() method when we use resp.redirect method
+const path = require('path')
+const filepath = path.join(__dirname,'index.ejs')
+app.set('view engine','ejs')
+app.set('views',filepath)
+const data = {
+    name :"mohit",
+    number:"6375349671"
+}
+
+app.route("/").get((req,resp)=>{
+    // resp.render(filepath,data,(err,html)=>{
+    //   resp.send(html)       it is optional to use
+    // }) 
+      resp.render(filepath,data)
 })
+
+
 app.listen(3000,()=>{
     console.log("api hit successfully")
 })
-
-
-
