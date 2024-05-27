ab80a1940d52b491ba328c4aa9e0be4d4c5be549
sharmasuper <mohit6375sharma@gmail.com>
2024-05-27 06:01:49 +0530

    resp.location method use

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const express = require('express')
 const app = express()
 
-app.route("/").get((req,resp)=>{
-    data = {
-        name:"mohit",
-        status:"success",
-        combine :{
-            name:"hello world",
-            today:new Date(),
-            ram :["ramm","hello","hy"]
-        }
-    }
-    resp.links({
-        frist : "http://localhost:3000/name=hello",
-        second : "http://localhost:3000/name=sunita",
-        last : "http://localhost:3000/?name=mohit"
-    })
-    resp.send(data)
+app.route("/location").get((req,resp)=>{
+    resp.location('http://google.com')  //when i triger the /location it should be redirecte the http:goole.com website
+    resp.status(302).send('Redirecting to http://example.com')
 })
 app.listen(3000,()=>{
     console.log("api hit successfully")
 })
+
+
+
+
+
