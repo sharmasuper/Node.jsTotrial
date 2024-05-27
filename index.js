7d45870ef714008e62b9448a063bc13311378bc8
sharmasuper <mohit6375sharma@gmail.com>
2024-05-27 11:08:30 +0530

    resp.sendfile() method

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-const path = require('path')
-const filepath = path.join(__dirname,'index.ejs')
-app.set('view engine','ejs')
-app.set('views',filepath)
-const data = {
-    name :"mohit",
-    number:"6375349671"
-}
+const path = require("path")
+// const filepath = path.join(__dirname,'History.txt')
+
 
 app.route("/").get((req,resp)=>{
-    // resp.render(filepath,data,(err,html)=>{
-    //   resp.send(html)       it is optional to use
-    // }) 
-      resp.render(filepath,data)
-})
 
+  const options = {
+    root : path.join(__dirname),
+    headers : {
+        'date':new Date(),
+         'recived':true
+    }
+    
+  }
+
+   resp.sendFile('Histor.txt',options,(err,html)=>{
+    if(err){
+        resp.status(400).send("file not found")
+    }else{
+        console.log("file send successfully with response")
+       
+    }
+   })
+})
 
 app.listen(3000,()=>{
     console.log("api hit successfully")
 })
+
