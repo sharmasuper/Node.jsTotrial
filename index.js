c13f9a029497904c86d81f4574ac1a85423804f2
sharmasuper <mohit6375sharma@gmail.com>
2024-05-27 06:07:56 +0530

    resp.location method use case who commentout

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express()
 app.route("/location").get((req,resp)=>{
     resp.location('http://google.com')  //when i triger the /location it should be redirecte the http:goole.com website
     resp.status(302).send('Redirecting to a http://example.com')
+    // resp.status(201).send('Redirecting to a http://example.com')
+    // resp.send('Redirecting to a http://example.com')
     // This is commonly used in conjunction with a 302 Found or 201 Created response to indicate the url
 })
 app.listen(3000,()=>{
