311d95a5ceb7076d02f2167a85eefab5df31fdb9
sharmasuper <mohit6375sharma@gmail.com>
2024-05-27 14:43:29 +0530

    resp.vary() method use

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
+const express = require("express")
+const app = express()
 
-// The res.type(type) method in Express.js is used to set the 
-// Content-Type HTTP header to the MIME type specified by the type parameter. 
-// This method can be useful for ensuring that the response is 
-// correctly interpreted by the client based on the type of content being sent.
+app.route("/content").get((req,resp)=>{
+    resp.vary('hello')
+  
+    if(req.accepts('json')){
+        resp.type('text').send('Hello, plain text!');
+    }
+    if(req.accepts('html')){
+        resp.send("<h1>hello, how vary</h1>")
+    }else if(req.accepts('json')){
+        resp.json({message :"hello josn !!"})
+    }else {
+        resp.type('text').send('Hello, plain text!');
+    }
+})
+
+app.get('/language', (req, res) => {
+    // Ensure the response varies based on the 'Accept-Language' header
+    res.vary('Accept-Language');
+  
+    const lang = req.acceptsLanguages('en', 'es');
+    if (lang === 'es') {
+      res.send('Hola, Mundo!');
+    } else {
+      res.send('Hello, World!');
+    }
+  });
 
 
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/html', (req, res) => {
-  res.type('html');
-  res.send('<h1>Hello World</h1>'); // Sends an HTML response
-});
+app.listen(3000,()=>{
+    console.log("api hit successfully")
+})
 
 
-app.get('/json', (req, res) => {
-  res.type('json');
-  res.send({ message: 'Hello World' }); // Sends a JSON response
-});
 
-app.get('/text', (req, res) => {
-  res.type('text');
-  res.send('Hello World'); // Sends a plain text response
-});
 
-app.get('/xml', (req, res) => {
-//  console.log( "hello type",res.type('hello'))  it send null ....
-res.type('xml')
-console.log(res.type('xml'))
-  res.send('<message>Hello World</message>'); // Sends an XML response
-});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+
+
+
+
+
+
 
