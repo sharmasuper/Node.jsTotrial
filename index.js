4d05813a3505391f59c2e875c253cbca90938dd9
sharmasuper <mohit6375sharma@gmail.com>
2024-05-28 22:17:05 +0530

    acceptsCharsets

--- index.js
+++ index.js
@@ -2,24 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Middleware to set the response charset based on client's preferences
+app.use((req, res, next) => {
+    const acceptedCharset = req.acceptsCharsets('iso-8859-1', 'windows-1252');
+
+    if (acceptedCharset) {
+        res.charset = acceptedCharset;
+    } else {
+        res.status(406).send('Not Acceptable');
+        return;
+    }
+    next();
+});
+
 // Define a route
 app.get('/data', (req, res) => {
-    // Check which content type the client accepts
-    const acceptedType = req.accepts(['json','html','xml']);
-    
-    // Respond based on the accepted type
-    if (acceptedType === 'json') {
-        res.json({ message: 'This is a JSON response' });
-    } 
-     else if (acceptedType === 'html') {
-        res.send('<html><body><h1>This is an HTML response</h1></body></html>');
-    } else if (acceptedType === 'xml') {
-        res.type('application/xml');
-        res.send('<response><message>This is an XML response</message></response>');
-    } else {
-        // Default response if none of the specified types are accepted
-        res.status(406).send('Not Acceptable');
-    }
+    res.send(`This response is encoded in ${res.charset}`);
 });
 
 // Start the server
