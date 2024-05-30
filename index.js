c942a275b9d294a496eac783b2b6f25add93c669
sharmasuper <mohit6375sharma@gmail.com>
2024-05-30 09:20:52 +0530

    req.param() method and default param use

--- index.js
+++ index.js
@@ -8,14 +8,13 @@ app.use(express.json());
 // Middleware to parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/data', (req, res) => {
-  if (req.is('application/json')) {
-    res.json({"hello":"frist"});
-  } else if (req.is('application/x-www-form-urlencoded')) {
-    res.send('Received URL-encoded data');
-  } else {
-    res.send('Received other type of data');
-  }
+app.get('/user/:id', (req, res) => {
+  // Deprecated way
+  const userId = req.param('id');
+  const token = req.param('token', 'defaultToken123');
+   // Providing a default value if token is not present
+   //it means  id is not present when we use default token 
+  res.send(`User ID: ${userId}, Token: ${token}`);
 });
 
 app.listen(port, () => {
