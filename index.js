446a6ef2cfe5886e3660f4fb15661eac344c4ac5
sharmasuper <mohit6375sharma@gmail.com>
2024-05-30 15:08:44 +0530

    req.cookies method

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const app = express();
+app.use(cookieParser())
 
-
-app.route("/post").post((req,resp)=>{
- const add = req.body.map((item,index)=>{
-         return `<p key=${index}>${item.name} and ${item.email}</p>`
-                
- })
-  
-  resp.send(`hello dude how data -  and bodyis ${req.body} hello bo - ${add}`) 
-
+app.route("/cookie").get((req,resp)=>{
+  resp.cookie('hy','hello',{
+    secure:true,
+    httpOnly:true,
+    path:'/cookie',
+    maxAge:2000
+  })
+  const getCookie = JSON.stringify(req.cookies)
+  resp.send(`successfully send cookie ${getCookie}`)
 })
 
-app.route('/home').get((req,resp)=>{
-  resp.send(`api need successfully - ${req.body}`)
+app.route("/get-cookie").get((req,resp)=>{
+  var getCookie = req.cookies
+  resp.send(`hello cookies - ${JSON.stringify(getCookie)}`)
 })
-
-app.listen(3000,()=>{
-  console.log("api hit successfully")
-})
-
-
-
-
-
-
-
-
+app.listen(3000, () => {
+  console.log("API hit successfully");
+});
