82fd54eb89d4dc58dca6c41963add0a161067d02
sharmasuper <mohit6375sharma@gmail.com>
2024-05-30 12:41:41 +0530

    req.baseUrl method use

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-// In Express.js, req.app is a property of the req (request) object that provides a reference to the Express
-//  application instance that is handling the request. This can be useful when you need to access application-level
-//   settings or methods from within your 
-// route handlers or middleware.
 const express = require('express')
 const app = express()
-app.set('appName','My Express App')
-app.use((req,resp,next)=>{
-  console.log(`Request recived at ${new Date().toISOString()}`)
-  next()
+const router = express.Router()
+
+router.route('/base').get((req,resp)=>{
+  url = req.baseUrl
+  ug = req.url
+  resp.send(`hello router url - ${url} and personal is ${ug} `)
+  // api hit url is /hello
 })
 
-app.route("/").get((req,resp)=>{
-  // const appName  = req.app.get('appName')
-  const appName = req.app.get('appName')
-  resp.send(`Welcome to ${appName} !`)
+app.use('/hello',router)
+
+app.route("/home").get((req,resp)=>{
+  hu = req.url
+  resp.send(`api hit url is  ${hu}`)
+  // api hit url is /home
 })
 
 app.listen(3000,()=>{
   console.log("api hit successfully")
 })
+
+
+
+
+
 
