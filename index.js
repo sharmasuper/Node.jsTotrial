905e0a5e1ba8ba3151a66eaa4b631691fce31932
sharmasuper <mohit6375sharma@gmail.com>
2024-05-31 22:58:15 +0530

    req.ip() method use

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.use((req,resp,next)=>{
-console.log("Hostname",req.hostname)
-next()
+app.use((req,res,next)=>{
+  console.log('Client IP',req.ip)
+  next()
 })
 
-app.get("/",(req,resp)=>{
-  if(req.hostname === 'localhost'){
-    resp.send("Hello from localhost") 
-  }else if(req.hostname === 'api.example.com'){
-    resp.send("Hello form api.example.com!")
-  }else{
-    resp.send("Hello From an unknow domain")
-  }
+app.get("/mohit",(req,resp)=>{
+  resp.send(`Hello my api Ip address is ${req.ip}`)
 })
-const port = 3000
 app.listen(3000,()=>{
-  console.log(`Server is running on port ${port}`)
-
+  console.log("api hit on port 3000")
 })
