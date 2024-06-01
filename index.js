7485583f680c3dcb50b341f65f4bcc02f681c3ca
sharmasuper <mohit6375sharma@gmail.com>
2024-06-01 17:19:42 +0530

    req.subdomain methd use

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-console.log("hello")
-// In Express.js, the req.fresh property is used to determine if the request is still 
-// fresh according to the HTTP caching mechanisms. It relies on request headers like If-Modified-Since and 
-// If-None-Match to determine whether the content has changed since the last request. If the resource is still fresh, 
-// the server can respond with a 304 Not Modified status, saving bandwidth by not sending the unchanged resource again.
-
-
 const express = require('express')
 const app = express()
 
-app.use((req,resp,next)=>{
-    resp.set('Last-Modified',new Date().toUTCString())
-    resp.set('ETag','12345')
-    next()
-})
-app.get('/check-cache',(req,resp)=>{
-    
-   if(req.fresh){
-      resp.send('The response is fresh')
-   }else if(req.stale){
-      resp.send('The response is stale')
-   }else {
-      resp.send('Unable to determine freshness')
-   }
+// app.use((req,res,next)=>{
+//     console.log("subdomain",req.subdomains)
+//     next()
+// })
+app.get('/',(req,resp)=>{
+    console.log(req.subdomains)
+    resp.send('Hello From the main domain !')
 })
 
-const port = 3000
-app.listen(port,()=>{
-   console.log("server is running on po")
+app.get('/sub',(req,resp)=>{
+    if(req.subdomains.length>0){
+        resp.send(`Hello from the subdomain :${req.subdomains.join('.')}`)
+
+    }else{
+        resp.send('No subdomain present')
+    }
 })
+app.listen(3000,()=>{
+    console.log("api hit successfully")
+})
+
+
+
+
+
+
+
+
+
 
 
