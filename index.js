43473187e3177b011d727e8d2383bd12ebc6f61c
sharmasuper <mohit6375sharma@gmail.com>
2024-06-01 13:01:58 +0530

    req.secure method

--- index.js
+++ index.js
@@ -1,56 +1,28 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-// Middleware to log route information
-app.use((req, res, next) => {
-    
-    if (req.route) {
-        console.log('Route: ',req.route);
-    } else { 
-        console.log('No route matched yet.'); 
-    }
-    next();
-});
-
-// Define a couple of routes
-app.get('/users/:id', (req, res) => {
-    res.send(`User ID: ${req.params.id}`);
-});
-
-app.post('/users', (req, res) => {
-    res.send('User created');
-});
-
-// Catch-all route for unmatched routes
-app.use((req, res) => {
-    res.status(404).send('Route not found');
-});
-
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
-
 const express = require('express')
 const app = express()
-const router = express.Router()
+const port = 3000
+const cookieparser = require('cookie-parser')
+app.use(cookieparser())
 
-router.get('/hy',(req,resp)=>{
-    console.log(req.route)
-    resp.send("hello route - "+req.route.path)
+app.get('/set-secure-cookie',(req,resp)=>{
+    resp.cookie('secureCookie','thisisASecureCookieValue',{
+        secure : true,
+        httpOnly : true,
+        maxAge : 1000*60*60
+    })
+    resp.send('Secure Cookie has been set')
 })
 
-app.use('/hello',router)
-
-app.get("/home",(req,resp)=>{
-    console.log("Home is  ",req.route)
-    resp.send("hello route how frist  "+req.route.path)
+app.get('/check-cookie',(req,resp)=>{
+    const secureCookie = req.cookies.secureCookie
+    if(secureCookie){
+        resp.send(`secure cookie value : ${secureCookie}`)
+    }else{
+        resp.send('NO secure cookie found')
+    }
 })
-
 app.listen(3000,()=>{
-    console.log("api hit successfully") 
+    console.log('Serevr is running on port 3000')
 })
-
 
 
