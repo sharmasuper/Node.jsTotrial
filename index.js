878da6de046ed34406f553d2a637d99e72a1c604
sharmasuper <mohit6375sharma@gmail.com>
2024-06-01 10:37:18 +0530

    req.protocol method

--- index.js
+++ index.js
@@ -2,20 +2,12 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log(`Requested path: ${req.path}`);
+    console.log(`Request protocol: ${req.protocol}`);
     next();
 });
 
-app.get('/user/:id', (req, res) => {
-    res.send(`User ID path: ${req.path}`);
-});
-
-app.get('/search', (req, res) => {
-    res.send(`Search path: ${req.path}`);
-});
-
-app.post('/login', (req, res) => {
-    res.send(`Login path: ${req.path}`);
+app.get('/', (req, res) => {
+    res.send(`Request protocol is: ${req.protocol}`);
 });
 
 app.listen(3000, () => {
