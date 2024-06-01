d78928d07da84682401c7fe76e699fdd68233e58
sharmasuper <mohit6375sharma@gmail.com>
2024-06-01 10:46:09 +0530

    req.query() method

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
-app.use((req, res, next) => {
-    console.log(`Request protocol: ${req.protocol}`);
-    next();
+// Define a route that uses req.query
+app.get('/search', (req, res) => {
+    // Retrieve query parameters
+    const query = req.query;
+    const term = query.term || 'No term provided';
+    const page = query.page || 1;
+
+    // Send a response
+    res.send(`Search Term: ${term}, Page: ${page}`);
 });
 
-app.get('/', (req, res) => {
-    res.send(`Request protocol is: ${req.protocol}`);
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
