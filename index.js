6b6ade9ed4ef42d1da146bba7c606461f623d3e4
sharmasuper <mohit6375sharma@gmail.com>
2024-06-01 16:25:15 +0530

    req.stale method

--- index.js
+++ index.js
@@ -1,43 +1,32 @@
-// In Express.js, req.signedCookies is used to access signed cookies. Signed cookies provide an additional
-//  layer of security by ensuring the integrity of the cookie value. To use signed cookies,
-//   you'll need to set up a secret for signing cookies.
-
-// Here’s an example demonstrating how to use req.signedCookies in an Express.js application:
-
-
+console.log("hello")
+// In Express.js, the req.fresh property is used to determine if the request is still 
+// fresh according to the HTTP caching mechanisms. It relies on request headers like If-Modified-Since and 
+// If-None-Match to determine whether the content has changed since the last request. If the resource is still fresh, 
+// the server can respond with a 304 Not Modified status, saving bandwidth by not sending the unchanged resource again.
 
 
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
 
-/// Replace this with your generated secret key
-const secretKey = '4c6f72656d497073756d446f6c6f7241696d65742e204c6f72656d497073756d446f6c6f724469676e697373696d20656c69742e2050726f696e206e696268';
+app.use((req,resp,next)=>{
+    resp.set('Last-Modified',new Date().toUTCString())
+    resp.set('ETag','12345')
+    next()
+})
+app.get('/check-cache',(req,resp)=>{
+    
+   if(req.fresh){
+      resp.send('The response is fresh')
+   }else if(req.stale){
+      resp.send('The response is stale')
+   }else {
+      resp.send('Unable to determine freshness')
+   }
+})
 
-app.use(cookieParser(secretKey));
+const port = 3000
+app.listen(port,()=>{
+   console.log("server is running on po")
+})
 
-// Route to set a signed cookie
-app.get('/set-signed-cookie', (req, res) => {
-    res.cookie('signedCookie', 'thisIsASignedCookieValue', {
-        signed: true, // Sign the cookie
-        httpOnly: true, // Ensure the cookie is not accessible via JavaScript
-        maxAge: 1000 * 60 * 60, // 1 hour
-    });  
-    res.send('Signed cookie has been set'); 
-});
 
-// Route to check if the signed cookie is set
-app.get('/check-signed-cookie', (req, res) => {
-    const signedCookie = req.signedCookies.signedCookie;
-    if (signedCookie) {
-        res.send(`Signed cookie value: ${signedCookie}`);
-    } else {
-        res.send('No signed cookie found');
-    }
-});
-
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
