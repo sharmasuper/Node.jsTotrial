cdab4711515f8e226d806345c1857d3152a86069
sharmasuper <mohit6375sharma@gmail.com>
2024-06-03 21:29:55 +0530

    express.text() middleware use

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
-// The express.static(root, [options]) method in Express.js is used to serve static files such as HTML files, 
-// images, CSS files, and JavaScript files. This middleware is useful for serving files from a specific directory. 
-// The root parameter specifies the root directory from which to serve static assets, and the optional options parameter can be 
-// used to customize the behavior of the middleware.
-
-// Here's an example demonstrating how to use express.static():
-
-
-
+// The express.text([options]) middleware in Express.js is used to parse incoming request bodies as text.
+//  This middleware is particularly useful when you
+//  need to handle plain text data sent in the body of HTTP requests.
 
 const express = require('express');
-const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const options = {
-    dotfiles: 'ignore',
-    etag: false,
-    extensions: ['htm', 'html'],
-    index: false,
-    maxAge: '1d',
-    redirect: false,
-};
+// Use express.text() middleware to parse text/plain content type
+app.use(express.text());
 
-// Serve static files with options
-// app.use(express.static(path.join(__dirname, 'public'),options));
-app.use(express.static(path.join(__dirname, 'public')))
-// Example route to serve an HTML file
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'Index.html'));
+// Example route to handle POST request with text data
+app.post('/textdata', (req, res) => {
+    const textData = req.body;  
+    console.log('Received text data:', textData);
+    res.send(`Received text data: ${textData}`); 
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
