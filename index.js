8e901cce7686f588b9319457dea45cf56ab0b863
sharmasuper <mohit6375sharma@gmail.com>
2024-06-03 17:22:06 +0530

    express.json() method

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-const express = require('express');
-const app = express();
-// In Express.js, the req.xhr property is a boolean value that is true if 
-// the request was made using an XMLHttpRequest (Ajax). This is useful for distinguishing between 
-// standard browser requests and requests made through JavaScript on the client side.
-// Route to demonstrate req.xhr
-app.get('/data', (req, res) => {
-    console.log("hello",req.xhr) 
-    if (req.xhr) {
-        // Request was made with XMLHttpRequest (Ajax)
-        res.json({ message: 'This is a response to an Ajax request' });
-    } else {
-        // Standard browser request
-        res.send('This is a response to a standard browser request');
-    }
-});
+const express = require('express')
+const app = express()
+const port = 3000
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.use(express.json())
+
+app.post('/data',(req,resp)=>{
+    const requestData = req.body;
+    console.log('Recived json data',requestData);
+    resp.send(`recived json data ${JSON.stringify(requestData)}`)
+})
+app.listen(3000,()=>{
+    console.log('api hit successfully')
+})
