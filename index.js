02c14e57a675ffde92ffe4d5bcce18281ece84fb
sharmasuper <mohit6375sharma@gmail.com>
2024-06-03 21:21:06 +0530

    express.static method example

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-const express = require('express')
-const authRoutes = require('./authRoutes')
-const userRoutes = require('./userRouter')
+// The express.static(root, [options]) method in Express.js is used to serve static files such as HTML files, 
+// images, CSS files, and JavaScript files. This middleware is useful for serving files from a specific directory. 
+// The root parameter specifies the root directory from which to serve static assets, and the optional options parameter can be 
+// used to customize the behavior of the middleware.
 
-const app = express()
-app.use('/auth',authRoutes)
-app.use('/user',userRoutes)
-
-
-const port = 3000
-
-app.listen(port,()=>{
-    console.log('server is runnig on port')
-})
+// Here's an example demonstrating how to use express.static():
 
 
 
 
+const express = require('express');
+const path = require('path');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const options = {
+    dotfiles: 'ignore',
+    etag: false,
+    extensions: ['htm', 'html'],
+    index: false,
+    maxAge: '1d',
+    redirect: false,
+};
+
+// Serve static files with options
+// app.use(express.static(path.join(__dirname, 'public'),options));
+app.use(express.static(path.join(__dirname, 'public')))
+// Example route to serve an HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'Index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
 
