2f79ded7f27f1137f2509c2ebfbf19b169d29b73
sharmasuper <mohit6375sharma@gmail.com>
2024-06-03 19:00:56 +0530

    express.raw() middleware

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+
+// The express.raw([options]) middleware in Express.js is used to parse incoming request bodies as a Buffer object. 
+// This is useful when you need to handle raw binary data, such as images or files, in your Express application.
+
 const express = require('express')
 const app = express()
+app.use(express.raw({ type: 'application/json' }))
+//issai hum buffer kai rup mai data get kartai h
+app.post('/rawdata',(req,resp)=>{
+    console.log('Raw data recived :',req.body)
+    resp.send('Raw data recived successfully'+JSON.stringify(req.body))
+})
+
 const port = 3000
-
-app.use(express.json())
-
-app.post('/data',(req,resp)=>{
-    const requestData = req.body;
-    console.log('Recived json data',requestData);
-    resp.send(`recived json data ${JSON.stringify(requestData)}`)
+app.listen(port,()=>{
+    console.log("server is running")
 })
-app.listen(3000,()=>{
-    console.log('api hit successfully')
-})
+
+
+
+
