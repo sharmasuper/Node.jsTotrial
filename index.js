344426ce81e1bc2c3cfbd789f1393b81db341e75
sharmasuper <mohit6375sharma@gmail.com>
2024-06-08 15:16:03 +0530

    app.disabled() method

--- index.js
+++ index.js
@@ -1,25 +1,63 @@
-// The app.disable(name) method in Express.js is used to disable a configuration setting identified by name. 
-// This method is the counterpart to app.enable(name), which enables a configuration setting.
-// Here's an example demonstrating how to use app.disable(name)
+// const express = require('express')
+// const app = express()
 
+// app.enable('x-powered-by')
+
+// app.use((req,resp,next)=>{
+//     const xPowerdByEnabled = app.get('x-powered-by')
+//     console.log(`x-powered-by enabled : ${xPowerdByEnabled}`)
+//     next()
+// });
+
+// app.disabled('x-powered-by');
+
+// app.get('/',(req,resp)=>{
+//     resp.send('hello world')
+// })
+
+// const port = 3000
+// app.listen(port,()=>{
+//     console.log(`Serever is running on port ${port}`)
+// })
+
+//second example 
 
 const express = require('express')
 const app = express()
-app.enable('x-powered-by');
 
-app.use((req,res,next)=>{
-    const xPoweredByEnabled = app.get('x-powered-by')
-    console.log(`'x-powered-by' enabled : ${xPoweredByEnabled}`);
+app.enable('trust proxy')
+
+app.use((req,resp,next)=>{
+    const trustproxyEnabled = !app.disabled('trust proxy')
+    console.log(`'trust proxy' enabled before disabled : ${trustproxyEnabled}`)
     next()
 })
-// app.disable('x-powered-by')
+app.disabled('trust proxy');
+app.use((req,resp,next)=>{
+const trustproxyEnabled = !app.disabled('trust proxy')
+console.log(`'trust proxy' enabled afetr disabled: ${trustproxyEnabled}`)
+next();
+})
 
 app.get('/',(req,resp)=>{
-    resp.send('Hello world')
+    resp.send('Hello world !')
 })
-const port = 3000;
-
+const port = 3000
 app.listen(3000,()=>{
-    console.log(`server is running on port ${port}`)
+    console.log("server is running on port 3000}")
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
