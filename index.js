16363e2187525e288c0d2c19a7d80400d6b7edb6
sharmasuper <mohit6375sharma@gmail.com>
2024-06-08 18:17:44 +0530

    app locals method

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
+// Sure! The app.locals object in Express.js is used to store
+//  variables that are accessible to all templates and routes within 
+//  the application. This is useful for defining global variables that
+//   you want to be available in 
+// your views.
+
 const express = require('express')
+const path = require('path')
 const app = express()
-app.set('appName','My express App')
-app.set('port',3000)
 
-app.get('/',(req,resp)=>{
-    const appName = app.get('appName')
-    const port = app.get('port')
-    const reGet = resp.get('port')
-    resp.send(`Welcome to appName ${appName} port is ${port} reGet 
-        is reget ${reGet}`)
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname,'views'));
 
+app.locals.siteTitle = 'My Awesome Site';
+
+app.get('/',(req,res)=>{
+    res.render('index',{message:"Welcome to the home page !"})
 })
 
-const port = app.get('port')
-app.listen(port,()=>{
+
+app.get('/about',(req,resp)=>{
+    resp.render('about',{message:"Learn more about us."})
+})
+
+const port = 3000
+
+app.listen(3000,()=>{
     console.log("api hit successfully")
 })
+
+
+
+
+
+
+
