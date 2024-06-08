f6421eff9757386b561c593cb951739600c8a791
sharmasuper <mohit6375sharma@gmail.com>
2024-06-08 19:04:26 +0530

    app.mountpath method use

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
-// The app.METHOD(path, callback [, callback ...]) method in Express.
-// js is used to define route handlers for HTTP methods (such as GET, POST, PUT, DELETE, etc.) at specified paths. 
-// METHOD is a placeholder for the actual HTTP method you want to handle, like app.get, app.post, app.put, app.delete, etc.
+
+// In Express.js, the app.mountpath property contains the path pattern(s) on which a sub-app was mounted. 
+// When you create a sub-application and mount it on a specific path, 
+// you can use app.mountpath to retrieve that path
 
 const express = require('express')
 const app = express()
-app.use(express.json())
-
+const subapp = express()
+subapp.use((req,resp,next)=>{
+    console.log('subapp Middleware ');
+    next()
+})
+subapp.get('/ms',(req,resp)=>{
+     resp.send('Hello from Subapp !')
+})
+app.use('/sub',subapp)
 app.get('/',(req,resp)=>{
-    resp.send('Get request to the homepage')
+    resp.send("Hello from Main App")
 })
-
-app.post('/submit',(req,resp)=>{
-    const data = req.body;
-    resp.send(`Post Method to /submit with date: ${JSON.stringify(data)}`)
+subapp.use((req,res,next)=>{
+    console.log(`Mounted on : ${subapp.mountpath}`)  //sub
+    next()
 })
-
-app.put('/update/:id',(req,resp)=>{
-    const id = req.params.id;
-    const data = req.body;
-    resp.send(`put request to /update/${id} with date : ${JSON.stringify(data)}`)
-
+const port = 3000;
+app.listen(port,()=>{
+    console.log('server is running on port 3000')
 })
-
-app.delete('/delete/:id',(req,res)=>{
-    const id = req.params.id
-    res.send(`Delete request to /delete/${id}`)
-})
-
-app.listen(3000,()=>{
-    console.log("api hit successfully")
-})
-
-
-
-
-
