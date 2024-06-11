a96a6414af3b2a87a5eaa01df6ae7e23a925bf46
sharmasuper <mohit6375sharma@gmail.com>
2024-06-11 17:25:12 +0530

    Writing error handler

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
-const express = require('express')
-const app = express()
+// In Express.js, you can write custom error handlers to handle different types 
+// of errors more gracefully. Error handlers are middleware functions that take four arguments: 
+// err, req, res, and next. Here are a few examples of different types of error handlers in an Express application.
 
-app.use(express.json())
+
+const Express = require('express')
+const app = Express()
+
+app.use(Express.json())
 
 app.get('/',(req,res,next)=>{
-    next(new Error('Something went wrong'))
+    const err = new Error('Something went wrong')
+    err.status = 400
+    next(err);
+})
+app.get('/error',(req,res,next)=>{
+    next(new Error('Another type of error!'))
 })
 
-app.use((err,req,next)=>{
-    // console.error("show error",err.stack);
-    res.status(500).send('something broke !')
+app.use((req,res,next)=>{
+    const err = new Error('Not found')
+    err.status = 404
+    next(err)
 })
+
+app.use((err,req,res,next)=>{
+    console.error(err.stack);
+    res.status(err.status || 500);
+    res.json({
+        message : err.message,
+
+    })
+})
+
 
 const port = 3000
 app.listen(3000,()=>{
-    console.log('Server is running on port')
+    console.log("server api hit successfully")
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
