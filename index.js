e51fb4eb939429a33713c0e5cfd608a7e896d766
sharmasuper <mohit6375sharma@gmail.com>
2024-06-11 17:11:23 +0530

    default error handler

--- index.js
+++ index.js
@@ -1,36 +1,20 @@
-// Handling errors effectively in an Express.js application involves using error-handling middleware
-//  to catch errors and respond appropriately. Below is an example that demonstrates how to handle errors, 
-// catch exceptions, and use custom error-handling middleware in Express.js.
-
-
 const express = require('express')
 const app = express()
+
 app.use(express.json())
 
-app.get('/error',(req,res,next)=>{
-    const error = new Error('This is a sample error')
-    error.status = 400
-    next(error)
-})
-app.get('/',(req,res)=>{
-    res.send('Hello ,world')
+app.get('/',(req,res,next)=>{
+    next(new Error('Something went wrong'))
 })
 
-app.use((err,req,res,next)=>{
-    console.error(err.stack);
-    res.status(err.status || 500).json({
-        status:'error',
-        message:err.message || 'Internal server Error'
-    })
+app.use((err,req,next)=>{
+    // console.error("show error",err.stack);
+    res.status(500).send('something broke !')
 })
 
+const port = 3000
 app.listen(3000,()=>{
-    console.log("spi hit successfully")
+    console.log('Server is running on port')
 })
-
-
-
-
-
 
 
