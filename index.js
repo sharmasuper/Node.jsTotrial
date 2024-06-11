2580e3fe05f98197faad2cce99fe8104a6817d3d
sharmasuper <mohit6375sharma@gmail.com>
2024-06-11 16:51:51 +0530

    error handling catch error

--- index.js
+++ index.js
@@ -1,39 +1,32 @@
-// Overriding the Express API in an Express.js application can be useful when you need to add 
-// custom behavior or modify the default behavior of Express.js methods. 
-// This technique involves extending or wrapping existing methods with your own functionality.
-
-// Below is an example demonstrating how to override the res.send method in an Express.js application to add 
-// a custom header to every response.
+// Handling errors effectively in an Express.js application involves using error-handling middleware
+//  to catch errors and respond appropriately. Below is an example that demonstrates how to handle errors, 
+// catch exceptions, and use custom error-handling middleware in Express.js.
 
 
 const express = require('express')
 const app = express()
-app.use((req,res,next)=>{
-    const orignalSend = res.send
-    res.send = function(body){
-   res.set('X-Custom-Header','This is a custom header')
-   return  orignalSend.call(this, body);
-    }
-   next()
+app.use(express.json())
+
+app.get('/error',(req,res,next)=>{
+    const error = new Error('This is a sample error')
+    error.status = 400
+    next(error)
+})
+app.get('/',(req,res)=>{
+    res.send('Hello ,world')
 })
 
-app.get('/',(req,resp)=>{
-    resp.send('Hello World !');
+app.use((err,req,res,next)=>{
+    console.error(err.stack);
+    res.status(err.status || 500).json({
+        status:'error',
+        message:err.message || 'Internal server Error'
+    })
 })
 
-
-
-const port = 3000
 app.listen(3000,()=>{
-    console.log('server is running on port-3000')
+    console.log("spi hit successfully")
 })
-
-
-
-
-
-
-
 
 
 
