f93f86aa6fd9a2f03429682645209fb2edf1ce7b
sharmasuper <mohit6375sharma@gmail.com>
2024-06-14 05:45:22 +0530

    connect mongodb to express js

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-// Configuring Express.js to work behind proxies involves setting up trust proxy 
-// settings to ensure that Express.js correctly handles requests forwarded by a proxy server. 
-// Here's an example of how to configure Express.js to work behind proxies:
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+const DB = 'mongodb+srv://ms6375349671:wLcS9TRwlil9rrLH@cluster0.ys4iioh.mongodb.net/mernStack?retryWrites=true&w=majority&appName=Cluster0'
+
+mongoose.connect(DB
+//  useNewUrlParse :true,
+//  useCreateIndex :true,
+//  useUnifiedTopology:true,
+//  useFindAndModify:false
+).then(()=>{
+  console.log('connection successfully')
+}).catch((err)=>{
+  console.log("show error",err)
+})
+
+const middleware = (req,res,next)=>{
+  console.log('Hello my mddleware')
+  next()
+}
+
+app.get("/",middleware,(req,res)=>{
+    res.send("api hit successfully")
+})
+app.listen(3000,()=>{
+  console.log("api hit successfully")
+})
 
 
-const express = require('express');
-const app = express();
 
-// Trust proxy settings
-app.set('trust proxy', true);
 
-// Middleware to parse JSON bodies
-app.use(express.json());
 
-// Sample route that logs client IP address
-app.get('/', (req, res) => {
-  const clientIP = req.ip; // Use req.ip to get the client's IP address
-  console.log('Client IP Address:', clientIP);
-  res.send('Hello from Express!');
-});
-
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 
 
