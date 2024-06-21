51dd4c981dda6f16c1c3444aaaa1c4cc11af3114
sharmasuper <mohit6375sharma@gmail.com>
2024-06-22 01:35:48 +0530

    mongoose totrial by web dev Simplified

--- app.js
+++ app.js
@@ -1,46 +1,58 @@
-const express = require('express')
 const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const route = require('./routes/authRoutes')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const { requireAuth, checkUser } = require('./middleware/authMiddleware')
-const app = express()
-app.use(bodyParser.json())
-// app.use(express.json())
-app.use(express.static('public'))
-app.use(cookieParser())
-app.set('view engine','ejs')
-
-dotenv.config()
-
-mongoose.connect(process.env.MONGO_URL,{}).then(()=>{
-    console.log("mongodb connect successfully")
-    app.listen(process.env.PORT,()=>{
-        console.log("application run successfully on port "+process.env.PORT)
-    })
-}).catch((error)=>{
-    console.log("Show connect error "+error)
+const User = require('./User')
+mongoose.connect("mongodb://localhost:27017/myNewDatabase").then(()=>{ 
+    console.log('mongodb Connected successfully')
+}).catch((err)=>{
+    console.log("mogo err",err)
 })
-//routes
-app.get("*",checkUser);
-app.get('/',(req,res)=>{
-    res.render('home') 
-}) 
+// const user = new User({name : 'Kayle',age : 26})
+// user.save().then(()=>console.log("User Saved successfully"))
+run()
+async function run(){
+    // const user = new User({name : 'Kayle',age : 26})  both method are same
+    // User.findById().save()
+    try{
+    // const user = await User.create(
+    //     {name : "kale",
+    //     age:27,
+    //     email : "Test@gmail.com",
+    //     hobbies : ["Weight Lifting ","Bowling"],
+    //     address : {
+    //         street : "123 Main St",
+    //     }
+    // }
+    // )
+    // // await user.save()  
+    // //change property
+    // //   user.name = 'Sally'
+    // user.createAt = 5
+    //   await user.save()
+    //  console.log(user)
+    //  console.log("seend saved ",user)
+//   const user =  await  User.findById("6675bde94e9a934ebc8637de")
+//const user = await User.find({name : 'kale'})     //issai id search nahi hogi
+ //const user = await User.find({name : 's'}) //output - [] Array
+//  const user = await User.exists({name : "kal"}) //id else null
+// const user = await User.deleteOne({name : "kale"})
+//const user = await User.where()  //all find Method who is mongodb and have deleted
+// const user = await User.where("name").equals("kale")
+//  const user = await User.where("age").gt("1").limit(2).select("age")
+ //user[0].bestFriend = "6675bde94e9a934ebc8637de"     //hum isko esai ref sai change kar skatai h
+//  user[0].save() 
+// const user = await User.where('age').gt(12).where("name").equals("kale").populate("bestFriend").limit(1) 
+// const user = await User.findOne({name : "Kayle"})
+// const user = await User.findByName("Kayle")
+// const user = await User.find().byName("Kayle")
+// const user = await User.where().find('Kayle')  //show error
+const user = await User.findOne({name:'Sally'})   //ager document mai email h to likhnai ki jarurath nahi padaigi
+  console.log("user find =>",user)
+  await  user.save()  hamai other document banakai save karna hoga because yai isami proper require field nahi h
+  console.log("saved User",user)
+  console.log("namedEmail is",user.namedEmail)  //output- mohit<mohit@gmail.com>
+//   user.sayHi()      
+}catch(err){
+    console.log("show schema err ",err)
+}  
+}
 
-app.get('/smoothies',requireAuth,(req,res)=>res.render('smoothies'))
-app.get('/set-cookies',(req,res)=>{
-   // res.setHeader('Set-Cookie','newUser=true')
-   res.cookie('newUser',false)
-   res.cookie('isEmployee',true,{maxAge:1000*60*60*24,httpOnly:true});
-   res.send('You got the cookies!');
 
-});
-app.get('/read-cookies',(req,res)=>{
-const cookies = req.cookies;
-console.log(cookies);
-res.json(cookies)
-
-})
-
-app.use(route)
