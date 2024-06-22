1281ad0ad494b2f277826ac5b52af0d253dce2a4
sharmasuper <mohit6375sharma@gmail.com>
2024-06-22 16:25:57 +0530

    custom _id create and other database related method

--- app.js
+++ app.js
@@ -1,27 +1,137 @@
 const mongoose = require('mongoose')
-const { createBlogPost ,getAllBlogPosts,getBlogPostById, updateBlogPost, deleteBlogPost} = require('./operations/MongoOperation')
-mongoose.connect("mongodb://localhost:27017/myNewDatabase").then(()=>{
-    console.log("mongodb connected successfully")
-})
-.catch((error)=>{
-    console.log("mongodb connected error ",error)
+const {Schema} = mongoose
+mongoose.connect('mongodb://localhost:27017/myNewDatabase').then(()=>{
+    console.log("mongoose connected successfully")
+}).catch((err)=>{
+    console.log("mongoose connected err",err)
 })
 
-// const newBlogPost = {
-//     title: "MOHIT title",
-//     author: "Mohit author",
-//     body: "MOHIT body",
-//     comments: [{ body: "mohit", date: new Date() }],
-//     date: new Date(),
-//     hidden: true,
-//     meta: {
-//       voters: 5,
-//       favs: 4
-//     }
-//   };
+//create schema 
+const customIdSchema = new Schema({
+    _id : Number  //_id : false means _id is disable
+})
 
-createBlogPost("mohit title","mohit auther","mohit body") 
-// getAllBlogPosts() 
-getBlogPostById("6676910fc24294acd470ad87")
-updateBlogPost("6676910fc24294acd470ad87","priranka title","priyanka author","priyanka body")
-deleteBlogPost("6676910fc24294acd470ad87")
+const customIdModel = mongoose.model('CustomIdModel',customIdSchema)
+
+
+async function customIdExample(_id){
+    try{
+        const customDoc = new customIdModel({_id});
+        await customDoc.save();
+              
+        console.log("id is that",customDoc._id) 
+
+    }catch(err){
+        console.log("show err custom Id",err)
+    }
+}
+
+customIdExample("10")
+
+/////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+// CHATGPT EXAMPLE  ---- chatgpt example ========\\\\
+///////////////////////////////////////////////////////////////
+
+
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+// Connect to MongoDB (update the URI with your connection string)
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Overwriting Mongoose's default _id with your own _id
+const customIdSchema = new Schema({
+  _id: Number // Overwrite Mongoose's default _id
+});
+
+const CustomIdModel = mongoose.model('CustomIdModel', customIdSchema);
+
+async function customIdExample() {
+  try {
+    const customDoc = new CustomIdModel();
+    // This will throw an error because _id is not set
+    await customDoc.save(); 
+  } catch (err) {
+    console.error('Error:', err.message); // document must have an _id before saving
+  }
+
+  const customDoc = new CustomIdModel({ _id: 1 });
+  await customDoc.save(); // Works because _id is set
+  console.log('Saved customDoc with _id:', customDoc._id);
+}
+
+// Disabling _id in subdocuments
+const nestedSchemaWithoutId = new Schema(
+  { name: String },
+  { _id: false } // Disable _id
+);
+
+const schemaWithSubdocs = new Schema({
+  subdoc: nestedSchemaWithoutId,
+  docArray: [nestedSchemaWithoutId]
+});
+
+const Test = mongoose.model('Test', schemaWithSubdocs);
+
+async function subdocsExample() {
+  await Test.create({
+    subdoc: { name: 'test 1' },
+    docArray: [{ name: 'test 2' }]
+  });
+
+  const result = await Test.findOne().lean();
+  console.log('Result:', result);
+}
+
+// Disable _id using alternative syntax
+const alternativeNestedSchemaWithoutId = new Schema({
+  _id: false, // Disable _id
+  name: String
+});
+
+const alternativeSchemaWithSubdocs = new Schema({
+  subdoc: alternativeNestedSchemaWithoutId,
+  docArray: [alternativeNestedSchemaWithoutId]
+});
+
+const AlternativeTest = mongoose.model('AlternativeTest', alternativeSchemaWithSubdocs);
+
+async function alternativeSubdocsExample() {
+  await AlternativeTest.create({
+    subdoc: { name: 'test 1' },
+    docArray: [{ name: 'test 2' }]
+  });
+
+  const result = await AlternativeTest.findOne().lean();
+  console.log('Result:', result);
+}
+
+// Run examples
+async function runExamples() {
+  await mongoose.connection.dropDatabase(); // Clear the database before running examples
+
+  await customIdExample();
+  await subdocsExample();
+  await alternativeSubdocsExample();
+
+  mongoose.disconnect();
+}
+
+runExamples().catch(err => console.error(err));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
