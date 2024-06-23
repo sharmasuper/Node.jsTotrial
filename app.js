3ccee06372577deb63634405f5a2e6f7dc4bf095
sharmasuper <mohit6375sharma@gmail.com>
2024-06-23 08:10:46 +0530

    Index method in mongoose

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-// Query Helpers in Mongoose allow you to add custom helper methods to your Mongoose models. 
-// These helpers are especially useful for reusing commonly used query operations. 
-// Here is an example to demonstrate how you can define and use Query Helpers in Mongoose.
-//Query Helpers
-const mongoose = require('mongoose');
+// Indexes in Mongoose (and MongoDB) are used to improve the efficiency of query operations
+//  by allowing the database to find and sort records faster.
+//  You can create indexes on fields to optimize queries and ensure unique constraints.
+// Here is an example demonstrating how to define indexes in Mongoose, including a unique index and a compound index.
+
+const mongoose = require('mongoose')
 const {Schema} = mongoose
 
 mongoose.connect('mongodb://localhost:27017/mydatabase')
@@ -10,46 +11,45 @@ mongoose.connect('mongodb://localhost:27017/mydatabase')
     console.log("mongoose connected successfully")
 })
 .catch((err)=>{
-    console.log('show mongose error ',err)
+    console.log("mongoose error ",err)
 })
 
 const userSchema = new Schema({
-    firstName : String,
-    lastName : String,
-    email : String
-});
+    firstName : {type : String,required : true },
+    lastName : {type : String,required : true},
+    email : {type : String ,required : true,unique : true},
+    age : {type: Number , index : true} //single field index on age
 
-userSchema.query.byFirstName = function (firstName){
-    return this.where({firstName : new RegExp(firstName,'i')}) ;
+})
 
-}
+//Compound index on firstName and LastName 
+userSchema.index({firstName : 1,LastName : 1})
+const User = mongoose.model('User',userSchema);
 
-const User = mongoose.model('User',userSchema) 
-
-//create function 
-
-async function findUsers(){
+async function run (){
     try{
-
+        await User.deleteMany({});
         await User.create([
-            { firstName: 'John', lastName: 'Doe', email: 'john@example.com' },
-            { firstName: 'Jane', lastName: 'Smith', email: 'jane@example.com' },
-            { firstName: 'Johnny', lastName: 'Appleseed', email: 'johnny@example.com' }
-          ]);
+            { firstName: 'John', lastName: 'Doe', email: 'john@example.com', age: 25 },
+            { firstName: 'Jane', lastName: 'Doe', email: 'jane@example.com', age: 28 },
+            { firstName: 'Jim', lastName: 'Beam', email: 'jim@example.com', age: 35 }
+          ]); 
+        
+       const userByEmail = await User.findOne({email : 'john@example.com'})
+       console.log('user found by email :',userByEmail) ;
+       
+       const userByAge = await User.find({age : {$gte : 30}});
+        console.log('User found by email :',userByAge)
 
-        // User the query Helper to find users by first name
-        const userNamedJohn = await User.find().byFirstName('john');
-        console.log("userNamedJohn ",userNamedJohn)   
-
+      const userByName = await User.findOne({firstName : 'Jane',lastName :'Doe'})
+      console.log('User found by first and last name : ',userByName)
 
     }catch(error){
-        console.log("Show method error ",error)
+        console.log("Show error ",error)
     }
 }
 
-findUsers()
-
-
+run()
 
 
 
