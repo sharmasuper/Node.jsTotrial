72c8d3134c2b7a3ec8abe61e16c24163cd2a9abd
sharmasuper <mohit6375sharma@gmail.com>
2024-06-24 12:44:33 +0530

    id method in mongoose

--- app.js
+++ app.js
@@ -1,43 +1,71 @@
-// The excludeIndexes option in Mongoose is used to prevent Mongoose
-//  from automatically building indexes defined in the schema. 
-//  This can be useful in situations where you want to manage indexes
-//   manually or when working with large collections where building 
-//   indexes automatically can cause performance issues.
-
-const mongoose = require('mongoose')
+// In Mongoose, the id method is used to retrieve a 
+// subdocument by its _id from an array of subdocuments within a 
+// document. This method is particularly useful when working with 
+// nested documents,
+//  such as an array of comments within a blog post
+const mongoose = require('mongoose');
 const {Schema} = mongoose
-mongoose.connect('mongodb://localhost:27017/test')
+
+mongoose.connect('mongodb://localhost:27017/mydatabase')
 .then(()=>{
-    console.log("mongoose connected successfully")
+    console.log('mongoose connected successfully')
 })
 .catch((error)=>{
-    console.log("mongoose error ",error)
+    console.log("mongoose connected  error ",error)
 })
 
-const userSchema = new mongoose.Schema({
-    name : {type : String,index : true},
-    email : {type : String,unique : true,index : true}
-},{excludeIndexes:true})
-
-const User = mongoose.model('users',userSchema)
-
-const runFunc = async() =>{
-    try{
-    const newData = new User({name : "Mohit ",email :"sharma@gmail.com"})
-    console.log(newData)
-    await newData.save()
-    console.log("data saved successfully")
-    //check indexes
-  const indexes = await User.listIndexes()
-    console.log("indexes ",indexes) 
+const commentSchema = new Schema({
+    text : String,
+    author : String
+});
 
 
-    }catch(error){
-      console.log("show error ",error)
-    }
+const PostSchema = new Schema({
+    title : String,
+    content : String,
+    comments : [commentSchema]
+})
+
+// Create the Post model
+const Post = mongoose.model('Post',PostSchema)
+
+const addPostAndFindComment = async() =>{
+try{
+   const newPost = new Post({
+    title : "My first Post",
+    content : 'This is the content of my first post',
+    comments : [
+        { text: 'Great post!', author: 'Alice' },
+        { text: 'Thanks for sharing!', author: 'Bob' }
+        //both id create different
+    ]
+   })
+  console.log(newPost)
+  await newPost.save()
+  console.log("docs saved successfully");
+  const commentId = newPost.comments[0]._id;
+  console.log("comment id is ",commentId)
+  //id method is use  for nested document 
+  const comment = newPost.comments.id(commentId);
+  console.log('Retrieved Comment:', comment);
+  await mongoose.connection.close();
+}catch(error){
+    console.log("show error ",error)
 }
+}
+addPostAndFindComment()
 
-runFunc()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
