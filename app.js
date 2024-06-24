07d8cdf1d1a3ddd7cde6e8496bdead16ea4b7023
sharmasuper <mohit6375sharma@gmail.com>
2024-06-24 11:06:15 +0530

    discriminatorKey method in mongoose

--- app.js
+++ app.js
@@ -1,38 +1,77 @@
-// The collection option in Mongoose allows you to specify the name of the collection that a model should use. 
-// By default, Mongoose will use the pluralized and lowercased version of the model name as the collection name. However,
-//  you can override this by explicitly setting the collection option.
+// The discriminatorKey option in Mongoose allows you to create multiple models that
+//  inherit from a base schema but have additional fields specific to each model.
+//   These models are stored in the same MongoDB collection, and the discriminatorKey field 
+// is used to distinguish between the different models.
 
-const mongoose = require('mongoose')
+
+const mongoose  = require('mongoose')
 const {Schema} = mongoose
 
 mongoose.connect('mongodb://localhost:27017/test')
 .then(()=>{
-    console.log("mongoose connect successfully")
+    console.log("mongoose connected successfully")
 })
 .catch((error)=>{
     console.log("show error ",error)
 })
 
+const options = {discriminatorKey : 'Kind'}
 const userSchema = new Schema({
-    name :String,
+    name : String,
     email : String
-},{
-   collection :'my_custom_collection' 
-})
-//
-//const User = new mongoose.model('users',userSchema,"my_custom_collection")
-const User = new mongoose.model('users',userSchema)
-//ab data users collection ki jagah my_custom_collection mai add hoga 
-const runFun = async() =>{
+},options)
+const User = mongoose.model('User',userSchema)
+
+const studentSchema = new mongoose.Schema({
+    major : String,
+    year : Number
+});
+const teacherSchema = new mongoose.Schema({
+    subject : String,
+    yearOfExperience : Number
+});
+
+//Create discriminators 
+
+const Student = User.discriminator('Student',studentSchema)
+const Teacher = User.discriminator('Teacher',teacherSchema)
+
+
+const runFunc = async() =>{
     try{
-     const newData = new User({name : "mohitji",email : "mohit@gmail.com"})
-     console.log(newData)
-     await newData.save()
-}catch(error){
-        console.log("show error ",error)
+    const newStudent = new Student({name : "Alice",email : 'alice@gmail.com'})
+    console.log(newStudent)
+    await newStudent.save() 
+    //second seceha teacher
+    const newTeacher = new Teacher({name: 'Bob',email : 'bob@gmail.com'})
+       console.log(newTeacher)
+    await   newTeacher.save()
+     //ek sath dono schema add hogai
+    //base user find
+
+    const users = await User.find();
+    console.log('All users:', users);
+
+      // Query only students
+      const students = await Student.find();
+      console.log('All students:', students);
+      //Query only teachers
+      const teachers = await Teacher.find();
+      console.log('All teachers:', teachers);
+
+
+    }catch(error){
+        console.log(error)
     }
 }
-runFun()
+
+runFunc()
+
+
+
+
+
+
 
 
 
