30519b9401685b02b085848298c999374c650392
sharmasuper <mohit6375sharma@gmail.com>
2024-06-24 13:16:12 +0530

    _id method  in mongoose

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-// In Mongoose, the id method is used to retrieve a 
-// subdocument by its _id from an array of subdocuments within a 
-// document. This method is particularly useful when working with 
-// nested documents,
-//  such as an array of comments within a blog post
+//The _id method is not a specific method in Mongoose. However, 
+// you might be referring to handling documents by their _id field, 
+// which is a unique identifier for each document in a 
+// MongoDB collection.
 const mongoose = require('mongoose');
 const {Schema} = mongoose
 
@@ -14,48 +13,36 @@ mongoose.connect('mongodb://localhost:27017/mydatabase')
     console.log("mongoose connected  error ",error)
 })
 
-const commentSchema = new Schema({
-    text : String,
-    author : String
-});
+const userSchema = new Schema({
+    name: String,
+    email: String
+  });
 
+const User = mongoose.model('User', userSchema);
 
-const PostSchema = new Schema({
-    title : String,
-    content : String,
-    comments : [commentSchema]
-})
+async function addUserAndFindById(){
+    try{
+      //Save the use to the databse 
+      const newUser = new User({
+        name: 'John Doe',
+        email: 'john.doe@example.com'
+      });
+      const savedUser = await newUser.save();
+      console.log('Saved User:', savedUser);
 
-// Create the Post model
-const Post = mongoose.model('Post',PostSchema)
+      const userId = savedUser._id;
+      const foundUser = await User.findById(userId);
+      console.log('Found User by _id:', foundUser);
+  
+      // Close the database connection
+      await mongoose.connection.close();
 
-const addPostAndFindComment = async() =>{
-try{
-   const newPost = new Post({
-    title : "My first Post",
-    content : 'This is the content of my first post',
-    comments : [
-        { text: 'Great post!', author: 'Alice' },
-        { text: 'Thanks for sharing!', author: 'Bob' }
-        //both id create different
-    ]
-   })
-  console.log(newPost)
-  await newPost.save()
-  console.log("docs saved successfully");
-  const commentId = newPost.comments[0]._id;
-  console.log("comment id is ",commentId)
-  //id method is use  for nested document 
-  const comment = newPost.comments.id(commentId);
-  console.log('Retrieved Comment:', comment);
-  await mongoose.connection.close();
-}catch(error){
-    console.log("show error ",error)
+    }catch(error){
+        console.log("show error ",error)
+    }
 }
-}
-addPostAndFindComment()
 
-
+addUserAndFindById()
 
 
 
