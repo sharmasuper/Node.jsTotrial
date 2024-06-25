d0240954d509c5f1c45a521066aadcc226cffd20
sharmasuper <mohit6375sharma@gmail.com>
2024-06-25 12:41:02 +0530

    toJSON() method in mongoose

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-// The strictQuery option in Mongoose controls how Mongoose handles 
-// fields that are not defined in the schema during query operations. 
-// When strictQuery is set to true, Mongoose will strip out any fields 
-// in the query that are not defined in the schema. When set to false, 
-// it allows querying with fields not defined in the schema.
+//The toJSON method in Mongoose is used to customize the JSON representation of documents when they are converted to JSON. This can be particularly useful for hiding sensitive information or transforming data before it is sent to clients.
 
 const mongoose = require('mongoose')
 const {Schema} = mongoose
@@ -14,51 +10,48 @@ mongoose.connect('mongodb://localhost:27017/test')
     console.log("show error ",error)
 })
 
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
     name : String,
-    category : String,
-    details : {
-        info : String,
-        extra : String
+    email : String,
+    password : String,
+},
+{
+    toJSON : {
+        virtuals : true , //Include virtuals 
+        versionKey : false,
+        transform : function (doc ,ret) {
+            delete ret._id, //Remove _id
+            delete ret.id //delete default Id
+            delete ret.password; //Remove password
+            return ret;
+           
+        }
     }
 })
+//Create a Model
+const User = mongoose.model('User',userSchema)
 
-mongoose.set('strictQuery',false) 
-//allow fields not defined in the schema for quries
-// by default false h yai matlab iskai true honai per vo field jo schema 
-//mai nahi h vo search kartha h yai
-const Example = mongoose.model('Example',userSchema)
-
-
-const runExample = async() =>{
-    try{
-  const example = new Example({
-    name : "example",
-    category : "example",
-    details : {
-        info : "example",
-        extra : "example"
-    }
-   
-  })
-
-  await example.save()
+const runfunc = async() =>{
+ try{
+    const user = new User({
+        name : 'John Doe',
+        email : 'mohit123@gmail.com',
+        password : 'secretpassword'
+     }) 
+      await  user.save()
+       console.log(user)
   
-  console.log(example)
-  console.log("user saved successfully")
-
- //find - query with a field not defined in the schema
-
- const findUser =  await  Example.find({nonExistentField: 'value'})
-console.log(findUser)
-
-}catch(error){
-    console.log("shoe error ",error) 
-}
-}
-runExample()
+     //find the document and convert into JSON
+     const findJsonUser = await User.findOne({email : 'mohit123@gmail.com' })
+      console.log("findJson user ",findJsonUser.toJSON())
+   
 
 
+ }catch(error){
+    console.log("show error ",error)
+ }
+} 
+runfunc()
 
 
 
