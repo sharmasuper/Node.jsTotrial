ab28a3caa6209cac1b9b461c4a9c716992d80f67
sharmasuper <mohit6375sharma@gmail.com>
2024-06-25 10:01:11 +0530

    wrireconcern method example

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-// The read option in Mongoose allows you to specify the read preference for queries, which can be useful in a replicated 
-// MongoDB setup. This option lets you define how the query should be distributed among the replica set members. Common read 
-// preferences include primary, 
-// primaryPreferred, secondary, secondaryPreferred, and nearest.
+// The writeConcern option in Mongoose allows you to specify the level of acknowledgment requested from MongoDB for write operations. This can be particularly important for ensuring data durability and consistency.
+
+// Write concern levels include:
+
+// w: The number of replicas that must acknowledge the write before it is considered successful.
+// j: If set to true, the write operation waits for the journal to be written to disk.
+// wtimeout: The timeout in milliseconds for write concern acknowledgment.
+
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/test')
@@ -9,46 +13,48 @@ mongoose.connect('mongodb://localhost:27017/test')
     console.log("mongoose connected successfully")
 })
 .catch((error)=>{
-    console.log("show error ",error)
+    console.log("show mongoose errr",error)
 })
 
-const userschema = new mongoose.Schema({
-    name : String,
+
+const Schema = new  mongoose.Schema({
+    name : String ,
     details : {
         info : String,
         extra : String
     }
-})
-
-const Example = mongoose.model('users',userschema)
-
-const runExample = async() =>{
-    try{
-    await Example.deleteMany({})
- const newDocs = new Example({
-    name : 'Example Name',
-    details : {
-        info : 'Some info',
-        extra : 'Some extra details'
-    }
- })
-
-    const result = await newDocs.save() 
-         console.log(result) 
-       const docs =   Example.find().read('secondaryPreferred')          
-       console.log(docs)
-    }catch(error){
-        console.log("shoe error ",error)
-    }
 }
+// {
+//    writeConcern : {
+//      w : 1,
+//      j : true,
+//      wtimeout : 1000
+//    }
 
-runExample()
+// }
+)
 
+const Example = mongoose.model('Example',Schema)
 
+const runFunc = async() =>{
+try{
+    const newDocs = new Example({
+        name : "Eample Name",
+        details : {
+            info : "Example Info",
+            extra : "Example Extra"
+        } 
+    })
+     //show user 
+     console.log(newDocs)
+     //save user
+     await newDocs.save()
+     console.log("save successfully")
+   
 
-
-
-
-
-
+}catch(error){
+    console.log("show error ",error)
+}
+}
+runFunc()
 
