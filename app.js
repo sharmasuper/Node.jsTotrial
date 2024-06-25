e20921c58c89601ccac51c64d122ed97ead80e5b
sharmasuper <mohit6375sharma@gmail.com>
2024-06-25 11:23:11 +0530

    Strict method in js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-// /In Mongoose, the shardKey option is used to specify the shard key 
-// for a collection in a sharded MongoDB cluster. The shard key is a
-//  field (or fields) used to distribute the data across the shards.
-//   Setting a shard key in Mongoose is done at the schema level.
+// The strict option in Mongoose defines how the schema behaves with 
+// fields that are not specified in the schema. By default, strict is 
+// set to true, meaning Mongoose will strip out any fields that are not
+//  defined in the schema. If strict is set to false, Mongoose will 
+//  allow the addition of fields that are not in the schema.
+
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/test')
@@ -13,45 +15,46 @@ mongoose.connect('mongodb://localhost:27017/test')
 })
 
 const exampleSchema = new mongoose.Schema({
+    _id : String,
     name : String,
     category: String,
     details : {
         info: String,
         extra : String
     }
-},{shardKey : {category:1}}) //Shard key on the category field
+},{strict:true}) // Allow fields not defined in the schema if true
 
 
 const Example = mongoose.model('Example',exampleSchema)
 
-const runfun = async() =>{
-    try{
-//   const newDocs = new Example({
-//     name : "Example Name",
-//     category : "Category A",
-//     details : {
-//         info : "Some Info",
-//         extra : 'Some extra details'
-//     }
-//   })
-//   console.log(newDocs) 
-// //saved user
-//   newDocs.save() 
-//find 
-const check = Example.find()
-console.log(check)
+const runFunc = async() =>{
+ try{
+  await Example.deleteMany({})
+  const example = new Example({
+      _id:1,          // id:'1', isisai kam nahi chalaiga _id hi lkhni hogi
+      name : "example1",
+      category : "category1",
+      details : {
+          info : "info1",
+          extra : "extra1"
+      },
+  },{
+    additionalField : "This field is not in the schema "
+  }
 
-  // Connect to the MongoDB instance via mongo shell and run:
-//whenever we find it 
-  // use test 
-// sh.enableSharding("test")
-// sh.shardCollection("test.examples", { category: 1 })
-
-}catch(error){
+)
+  await example.save()
+ 
+ const find =  await Example.find({}) 
+ console.log(find)
+ }catch(error){
     console.log("show error ",error) 
-}  
+ }
 }
-runfun()
+
+runFunc()
+
+
 
 
 
