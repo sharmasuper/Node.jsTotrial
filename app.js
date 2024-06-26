da95860b766943008fd1ca15ce57cd73cdd63237
sharmasuper <mohit6375sharma@gmail.com>
2024-06-26 12:43:06 +0530

    optimisticConcurrency method in mongoose

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-// In Mongoose, the versionKey option is used to specify the version key's name in a schema. 
-// By default, Mongoose adds a __v property to documents to keep track of the document version for 
-// concurrency control. You can change the name of this property using the versionKey option or disable it entirely.
+// Optimistic concurrency control is a technique used to handle concurrent updates to a document in Mongoose. 
+// It ensures that changes to a document are not overwritten by concurrent updates. Mongoose uses the version key (__v by default) for 
+// optimistic concurrency control. When optimisticConcurrency is set to true, Mongoose will check the version key to detect if the document has
+// been modified between when it was retrieved and when it was saved.
 
 const mongoose = require('mongoose') 
 const {Schema} = mongoose   
@@ -18,7 +19,7 @@ const userSchema = new Schema({
     age : {t : Number ,required : true },
     email : {t : String ,required : true},  
 },
-{typeKey : customTypeKey,versionKey : 'myVersionKey'}
+{typeKey : customTypeKey,versionKey : 'myVersionKey', optimisticConcurrency: true}
  //custom version key
 )
 
@@ -29,7 +30,7 @@ const runFun = async() =>{
     try{
             // await User.deleteMany()
        newData = new User({name : "Mohit Sharma" ,
-        age : 25 , email : 'MohitType1m3@gmail.com'})
+        age : 25 , email : 'MohitType1mB3@gmail.com'})
        await newData.save() 
        console.log(newData)
     }catch(error){
