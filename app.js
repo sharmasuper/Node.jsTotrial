64f84452e952accaed295a61dba621b5810f0965
sharmasuper <mohit6375sharma@gmail.com>
2024-06-26 12:15:42 +0530

    validateBeforeSave in mongoose

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// n Mongoose, the typeKey option allows you to specify a different 
-// key name for defining types in a 
-// schema. By default, Mongoose uses type as the key for type 
-// definitions, but this can be customized using the typeKey option
-
+// /The validateBeforeSave option in Mongoose can be used to 
+// disable automatic validation before saving a document. By 
+// default, Mongoose validates documents before saving them to the 
+// database. However, you can change this behavior by setting 
+// validateBeforeSave to false.
 
 
 const mongoose = require('mongoose')
@@ -25,11 +25,18 @@ const User = mongoose.model('User',userSchema)
 
 const runFun = async() =>{
     try{
-       newData = new User({name : "Mohit Sharma" ,age : 25 , email : 'MohitType@gmail.com'})
-       await newData.save()
+            // await User.deleteMany()
+       newData = new User({name : "Mohit Sharma" ,age : 25 , email : 'MohitType1@gmail.com'})
+       await newData.save({ validateBeforeSave: true }) 
        console.log(newData)
     }catch(error){
         console.log("show error ",error)
     }
 }
 runFun()
+
+
+
+
+
+
