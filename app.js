69ce10a5a9a5b43e6bcddff575bf31e32c613933
sharmasuper <mohit6375sharma@gmail.com>
2024-06-26 19:06:57 +0530

    selectPopulatedPaths in mongoose very important example in mongoose

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-// Mongoose plugins allow you to encapsulate reusable schema logic.
-//  You can define a plugin to handle specific functionality, 
-//  such as managing tags on documents. Here’s an example of how to 
-//  create a plugin for adding tags to Mongoose documents.
+// The selectPopulatedPaths method in Mongoose allows you to 
+// selectively populate certain paths of a document. 
+// This is useful when you want to control which fields get 
+// populated based on specific conditions.
+
 
 const mongoose = require('mongoose') 
-const { pluginTags } = require('./pluginTags')
 const {Schema} = mongoose   
+
 mongoose.connect('mongodb://localhost:27017/test') 
 .then(()=>{ 
     console.log("mongoose connected successfully")
@@ -14,40 +15,59 @@ mongoose.connect('mongodb://localhost:27017/test')
     console.log("show error ",error)
 })
 
-const blogPostSchema = new mongoose.Schema({
+const authorSchema = new Schema({
+    name : String,
+    bio : String
+})
+
+const bookSchema = new Schema({
     title : String,
-    content : String,
-    auther : String
-},{timestamps:true}) //Adds createAt and upodateAt fields
+    author : {
+        type : Schema.Types.ObjectId,
+        ref : 'Author'
+    },
+    content : String
+})
 
+const Author = mongoose.model('Author',authorSchema)
+const Book = mongoose.model('Book',bookSchema)
 
-blogPostSchema.plugin(pluginTags)
-const BlogPost = mongoose.model('sensorReadings',blogPostSchema)
+const runFun = async() =>{ 
+   try{
+    await Author.deleteMany({})
+    await Book.deleteMany({}) 
+     const author = new Author ({
+        name : "John Doe",
+        bio : "John is seasoned author with multiple bestsellers"
+     });
+     await author.save()
+     //book user 
+     const book = new Book({
+        title : 'First Book',
+        author : author._id,
+        content : 'This is the content of the first book'
+     });
+     await book.save() 
+     
+     console.log(book)
+    //control which field get populated 
+    const findBook = await Book.findOne({title : "First Book"}).populate('author')
+    //jab mai populate nahi likuga tab mujai user ki id melegi or jab likuga tab user ka docs melega
+  //second method
+  // const findBook = await Book.findOne({title:"First Book"}).populate({path:'author',select:'name'});
+  console.log("findBook schema",findBook) 
+    //esai jo select karai vo milaga chahai id or name or bio
+    //populate only the name field of the auther
+    // const populatedBook = await findBook.selectPopulatedPaths('author', 'name').execPopulate()
+    //  console.log("Populated book with selected paths ",populatedBook)
+    // Populate only the name field of the author
+    const findBooksSpecial = await Book.findOne({title:"First Book"}).populate({path:'author',select:['name','bio']});
+    
+    console.log("Populated book with selected paths ",findBooksSpecial)
+   //esai bhi kar sktai h
+   }catch(error){
+    console.log("shoe error ",error)
+   }
 
-
-const runFun = async() =>{
-try{
-   const post = new BlogPost({
-    title : "First Post",
-    content : "This is the content of the post",
-    author : 'John Doe'
-   })
-    await  post.save()   
-   console.log("post saved ",post)
-     //add another tag 
-     await post.addTag('Programming');
-   console.log("add post ",post)
-
-   //remove a tag
-   await post.removeTag('Programming');
-   console.log("remove post ",post)
-
-   //get all tags
-  const tags = await post.getTags();
-  console.log(tags)
-
-}catch(error){
-        console.log("show error ",error) 
-    }
 }
 runFun()
