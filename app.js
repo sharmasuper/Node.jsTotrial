e1b3ffa101cdffafd03e097860c3bd45859eed16
sharmasuper <mohit6375sharma@gmail.com>
2024-06-28 12:32:28 +0530

    autoIndex method

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-// In Mongoose, collectionOptions allows you to specify options when 
-// creating a collection. These options are typically used to set 
-// various collection-level configurations such as collation, capped 
-// collections, and more.
+// /The autoIndex option in Mongoose is used to specify whether or 
+// not Mongoose should automatically build indexes defined in your 
+// schema when your application starts. By default, autoIndex is set 
+// to true in development mode and false in production mode. However,
+//  you can override this behavior based on your specific requirements.
+
 
 const mongoose = require('mongoose');
 const {Schema} = mongoose
@@ -9,16 +11,16 @@ mongoose.connect('mongodb://localhost:27017/test')
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Connection error', err));
 
-  const schemaOptions = {
-    collation: { locale: 'en', strength: 2 }, // Collation option
-    capped: { size: 1024, max: 100 }, // Capped collection option
-  };
+//   const schemaOptions = {
+//     collation: { locale: 'en', strength: 2 }, // Collation option
+//     capped: { size: 1024, max: 100 }, // Capped collection option
+//   };
 
 
 const userSchema = new Schema({
     name : {type : String,required : true},
     age : {type : Number,required:true}
-},schemaOptions)
+},{autoIndex:true})
   
 
 const User = mongoose.model('User',userSchema,'Ms')
