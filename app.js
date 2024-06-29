bcdb14e6c0f503ec670b197a6396e40d1a878d9d
sharmasuper <mohit6375sharma@gmail.com>
2024-06-29 16:22:08 +0530

    chnageStream example

--- app.js
+++ app.js
@@ -1,56 +1,51 @@
-// Sure! Here is the full code that includes the getter method for the picture field in a Mongoose schema, along with the ability to control whether getters 
-// are applied when converting the document to an object.
+// To work with change streams in MongoDB using Mongoose, 
+// you need to be connected to a MongoDB replica set. Change streams
+//  allow you to listen to real-time updates on your collections.
+//   Below is an updated example of how you can set up and use change 
+//   streams with Mongoose.
 
-const mongoose = require('mongoose');
-const {Schema} = mongoose
-const path = require('path')
-const rootpath = path.join(__dirname,'operations')
-// const root = 'https://s3.amazonaws.com/mybucket'
-console.log(rootpath)
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/mydatabase').then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.error('Error connecting to MongoDB', err);
-});
 
-// Define a schema
 
-const userSchema = new Schema({
-  name : String,
-  picture : {
-    type : String,
-    get : v => `${rootpath}${v}`
-  }
+// K7g50h7PieDU1TPM
+// ms6375349671
+const mongoose = require('mongoose')
+const {Schema}  = mongoose
+const str = "mongodb+srv://ms6375349671:K7g50h7PieDU1TPM@cluster0.pyjdzrd.mongodb.net/mernStack?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect(str).then(()=>{
+  console.log("mongoose connected successfully")
 })
-// Apply the getters to the JSON and Object output by default
-userSchema.set('toJSON',{getters: true});
-userSchema.set('toObject',{getters : true});
+.catch((error)=>{
+  console.log("show error ",error)
+})
 
 
-const User =  mongoose.model('User',userSchema)
 
-async function run(){
+const userschema = new Schema({
+  name : String,
+  
+})
+
+const Person = mongoose.model('Person',userschema)
+
+const runfunc = async() =>{
   try{
-  //  const newDoc = new User({
-  //   name : "bollywood Actress",
-  //   picture : '/katrina.jpg'
-  //  })
-  //  console.log(newDoc)
-  //console.log("show docs",newDoc.toObject({ getters: false }).picture)
-  //console.log(doc.toObject().picture)
-  //give all link
-  ///Katrina.jpg
-  //  await newDoc.save()
+     //create a change stream and listen for changes
+     const changeStream = Person.watch()
+   
+     changeStream.on('change',data =>{
+      console.log(new Date(),data)
+     });
+    //Insert a document to trigger the change stream
+    console.log(new Date() , 'Inserting doc');
+    const newDocs =  new Person({name :"Axi Rose"});
+        console.log(newDocs)
+    await  newDocs.save() 
+     console.log("person saved successfully") 
 
-   const  findUser = await  User.find({name:'bollywood Actress'})
-   console.log(findUser)
-   console.log(findUser.toJSON())
-  // console.log(findUser.toObject({ getters: true }).picture) 
-    //yai wrong h cpde 
   }catch(error){
     console.log("show error ",error)
   }
 }
-run()
 
+runfunc()
