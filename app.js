db00b4ee60c62ff1ca899d86172f0e8f50584519
sharmasuper <mohit6375sharma@gmail.com>
2024-07-03 16:05:49 +0530

    doubleArray sai bhi docs add kar sktai h

--- app.js
+++ app.js
@@ -1,51 +1,56 @@
-// To work with change streams in MongoDB using Mongoose, 
-// you need to be connected to a MongoDB replica set. Change streams
-//  allow you to listen to real-time updates on your collections.
-//   Below is an updated example of how you can set up and use change 
-//   streams with Mongoose.
-
-
-
-// K7g50h7PieDU1TPM
-// ms6375349671
+//K7g50h7PieDU1TPM
 const mongoose = require('mongoose')
-const {Schema}  = mongoose
-const str = "mongodb+srv://ms6375349671:K7g50h7PieDU1TPM@cluster0.pyjdzrd.mongodb.net/mernStack?retryWrites=true&w=majority&appName=Cluster0"
+const {Schema} = mongoose
 
-mongoose.connect(str).then(()=>{
+mongoose.connect('mongodb+srv://ms6375349671:K7g50h7PieDU1TPM@cluster0.wv4kfmu.mongodb.net/MERNFirst?retryWrites=true&w=majority&appName=Cluster0')
+.then(()=>{
   console.log("mongoose connected successfully")
 })
 .catch((error)=>{
   console.log("show error ",error)
 })
 
+//Define User Schema with autoCreate and autoIndex disabled
 
+//jab hum single data schema add kartai h tab hamai pipeline ki 
+//need nahi hoti but jab ek array mai data insert kartai h tab hamai 
+//pipeline ki need hoti h 
 
-const userschema = new Schema({
+const userSchema = new Schema({
   name : String,
-  
-})
+  email : String,
+   roles: [String]
+},{autoCreate:false,autoIndex:false})
 
-const Person = mongoose.model('Person',userschema)
 
-const runfunc = async() =>{
+const User = mongoose.model('Ram',userSchema)
+// const RedactedUser = mongoose.model('RedactedUser',userSchema)
+
+
+const createView = async() =>{
   try{
-     //create a change stream and listen for changes
-     const changeStream = Person.watch()
-   
-     changeStream.on('change',data =>{
-      console.log(new Date(),data)
-     });
-    //Insert a document to trigger the change stream
-    console.log(new Date() , 'Inserting doc');
-    const newDocs =  new Person({name :"Axi Rose"});
-        console.log(newDocs)
-    await  newDocs.save() 
-     console.log("person saved successfully") 
+ 
 
+ const newData =  await User.create(
+  [{name : "RammohitTannu gandhi",email : 'mohitgandhi2@gmail.com',roles : ["mohhyuc","fattu2"]
+  },
+  {name :"RammohitTannu",email : "mohit@gmail.com",roles : ["soniya2",'soniya1']}
+])
+  
+  console.log(newData)
   }catch(error){
     console.log("show error ",error)
   }
 }
 
-runfunc()
+createView()
+
+
+
+
+
+
+
+
+
+
