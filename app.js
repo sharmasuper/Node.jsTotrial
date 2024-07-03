b8c3758881b62349e37566c4ccb09fafcfb8c11f
sharmasuper <mohit6375sharma@gmail.com>
2024-07-03 19:56:57 +0530

    userSchema.per() middleware use

--- app.js
+++ app.js
@@ -14,33 +14,47 @@ mongoose.connect('mongodb+srv://ms6375349671:K7g50h7PieDU1TPM@cluster0.wv4kfmu.m
 
 
 const childSchema = new Schema({
-  name : {
-    type : String,
-    required : true
-  }
+  name : String
+})
+childSchema.pre('validate',function (next){
+  console.log('child validate 2 call')
+  next()
 })
 //only have pre method like - pre('validate') or pre('save)
-childSchema.pre('save',function(next){
-  if(this.name === 'invalid'){
-    return next(new Error("#sadpanda"))
-  }
+// childSchema.pre('save',function(next){
+//   if(this.name === 'invalid'){
+//     return next(new Error("#sadpanda"))
+//   }
+//   next()
+//  });
+ childSchema.pre('save',function(next){
+  console.log("3 save detaction child save")
   next()
- });
- const parentSchema = new Schema({
-  children : [childSchema]
  })
- const Parent = mongoose.model('Parent',parentSchema);
+ 
+ const parentSchema = new Schema({
+  child : childSchema
+ })
+
+ parentSchema.pre('validate',function(next){
+  console.log("123 parent validate call ")
+  next()
+ })
+ parentSchema.pre('save', function(next) {
+  console.log('4 parent save');
+  next();
+});
+const Parent = mongoose.model('Parent',parentSchema)
 const runfunction = async() =>{
  try{
      
-     const parent = await  Parent.create({
-      // children : [{name : 'helllo'}]  //no Error
-      children : [{name : "invalid"}]  // show error
-
-     })
-     console.log(parent)
-     
-
+    const parent =  await Parent.create({
+      child : {name : "example"}
+    })
+    // await parent.validate()
+    // console.log("parent validate")
+    // await parent.save()
+    // console.log("Parent saved")
     
  }catch(error){
   console.log("show error ",error)
