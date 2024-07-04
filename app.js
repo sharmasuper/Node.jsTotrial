df301d9cedf7d5ee7cec425114a6ce07a54bf809
sharmasuper <mohit6375sharma@gmail.com>
2024-07-04 19:46:12 +0530

    populate path importnt example h

--- app.js
+++ app.js
@@ -1,52 +1,94 @@
 //K7g50h7PieDU1TPM
-const mongoose = require('mongoose')
-const { Schema, Types } = mongoose;
+//ms6375349671
 
-mongoose.connect('mongodb+srv://ms6375349671:K7g50h7PieDU1TPM@cluster0.wv4kfmu.mongodb.net/MERNFirst?retryWrites=true&w=majority&appName=Cluster0')
-.then(()=>{
-  console.log("mongoose connected successfully")
+const { default: mongoose, Schema } = require("mongoose");
+
+const uri1 = 'mongodb+srv://ms6375349671:K7g50h7PieDU1TPM@cluster0.wv4kfmu.mongodb.net/db1?retryWrites=true&w=majority&appName=Cluster0'
+const uri2 = 'mongodb+srv://ms6375349671:K7g50h7PieDU1TPM@cluster0.wv4kfmu.mongodb.net/db2?retryWrites=true&w=majority&appName=Cluster0';
+
+const db1 = mongoose.createConnection(uri1)
+const db2 = mongoose.createConnection(uri2)
+
+
+db1.on('connected',()=>{
+  console.log("Connected to db1")
+
 })
-.catch((error)=>{
-  console.log("show error ",error) 
+db1.on('error',()=>{
+  console.error("db1 is not connected show error")
+})
+db2.on('connected',()=>{
+  console.log('connected to db2')
 })
 
-const conversationSchema = new Schema({numMessage : Number});
-//issai hum different database bana sktai h
-const Conversation = mongoose.model('Conversation', 
-  conversationSchema, 'conversations', { db: 'db2' });
+db2.on('error',()=>{
+  console.error("db1 is not connected show error")
+})
+
+
+const conversationSchema = new Schema({numMessage :Number})
+const Conversation = db2.model('Conversation',conversationSchema)
 
 
 const eventSchema = new Schema({
   name : String,
   conversation : {
-    type : Types.ObjectId,
-    ref :  'Conversation'
+    type : Schema.Types.ObjectId,
+    ref : 'Conversation'
   }
-})
+});
 
-const Event = mongoose.model('Event', eventSchema, 
-  'events', { db: 'db1' });
 
+const Event = db1.model('Event',eventSchema);
 
 async function createEventWithConversation(){
   try{
-   
-   const newConversation = new Conversation({numMessage:0});
-   await newConversation.save()
-
-   const newEvent = new Event({
-    name : 'Sample Event',
-    conversation : newConversation._id
-   });
+    const newConversation = new Conversation({numMessage :0})
+    await newConversation.save()
     
-   await newEvent.save();
-  console.log("Event and Conversation created successfully");
+    const newEvent = new Event({
+          name : "Sample Event",
+          conversation : newConversation._id
+    });
+    // console.log(newEvent)
+    await newEvent.save()
+    //Create a new Event in db1 with a reference to the doversation 
 
+    // console.log('Event and conversation created successfully')
+    // //find population 
+    const event = await Event.findById(newEvent._id).populate({ 
+      path: 'conversation',
+      model: Conversation
+    }).exec();
+    console.log('Event with populated conversation:', event);
   }catch(error){
-    console.log("show error ",error)
+    console.error(error)
   }
 }
 
 createEventWithConversation()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
