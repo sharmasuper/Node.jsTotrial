56e537979e6b99e5e2f38ce0f08518ba65d2f9ec
sharmasuper <mohit6375sharma@gmail.com>
2024-07-04 11:34:04 +0530

    aggregation method use

--- app.js
+++ app.js
@@ -9,64 +9,48 @@ mongoose.connect('mongodb+srv://ms6375349671:K7g50h7PieDU1TPM@cluster0.wv4kfmu.m
 .catch((error)=>{
   console.log("show error ",error) 
 })
-// Certainly! The pre method in Mongoose is a middleware that allows you to run some function before a particular operation (like save, validate, remove, etc.) is executed. In this case, 
-// you want to execute a function before a child document is saved.
 
-
-const childSchema = new Schema({
-  name : String
+const personSchema = new Schema({
+  region : String,
+  sales : Number
 })
-childSchema.pre('validate',function (next){
-  console.log('child validate 2 call')
-  next()
-})
-//only have pre method like - pre('validate') or pre('save)
-// childSchema.pre('save',function(next){
-//   if(this.name === 'invalid'){
-//     return next(new Error("#sadpanda"))
-//   }
-//   next()
-//  });
- childSchema.pre('save',function(next){
-  console.log("3 save detaction child save")
-  next()
- })
- 
- const parentSchema = new Schema({
-  child : childSchema
- })
 
- parentSchema.pre('validate',function(next){
-  console.log("123 parent validate call ")
-  next()
- })
- parentSchema.pre('save', function(next) {
-  console.log('4 parent save');
-  next();
-});
-const Parent = mongoose.model('Parent',parentSchema)
-const runfunction = async() =>{
- try{
-     
-    const parent =  await Parent.create({
-      child : {name : "example"}
-    })
-    // await parent.validate()
-    // console.log("parent validate")
-    // await parent.save()
-    // console.log("Parent saved")
-    
- }catch(error){
-  console.log("show error ",error)
- }finally{
-  mongoose.connection.close()
- }
+
+const Sale = mongoose.model('Sales',personSchema)
+
+async function streamDocument(){
+  try{
+  //   const newData = await Sale.create([
+  //     { "region": "North", "sales": 100 },
+  // { "region": "South", "sales": 150 },
+  // { "region": "North", "sales": 200 },
+  // { "region": "East", "sales": 250 },
+  // { "region": "West", "sales": 300 },
+  // { "region": "South", "sales": 100 }
+  //   ])
+   const results = await Sale.aggregate([
+      {
+        $group : {
+          _id : "$region",
+          totalSales : { $sum : "$sales" }
+        }
+      },
+      {
+        $sort : {totalSales:1}
+      }
+   ])
+
+   console.log(results)
+   
+  }catch(error){
+    console.log('Error streaming document :',error)
+  }
+  finally{
+    mongoose.connection.close()
+  }
 }
 
-runfunction()
-
-
-
+streamDocument()
 
 
 
