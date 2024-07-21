9f0c192b3075b879f696582eddeca1d376952b00
sharmasuper <mohit6375sharma@gmail.com>
2024-07-21 20:17:01 +0530

    multer saving local file on local server

--- Index.js
+++ Index.js
@@ -1,59 +1,32 @@
-const mongoose = require('mongoose')
-const env = require('dotenv')
 const express = require('express')
-const { User } = require('./UserModel/model')
 const app = express()
-env.config()
+const multer = require('multer')
+const env = require('dotenv')
+// const upload = multer({ dest:  'uploads/' })
 
-mongoose.connect(process.env.Str)
-.then(()=>{ 
-    app.listen(process.env.port,()=>{
-        console.log("api connected successfully  "+process.env.port )
-    })
-})
-.catch((error)=>{
-    console.log("show mongoose error ",error)
-})
+env.config() 
 
-app.use(express.json())
-app.use(express.static('public')) 
-app.set('view engine','ejs') 
- 
-const paginationFunctionality = (StaticData) =>{
-    return async (req,res,next) =>{
-        const page = parseInt(req.query.page,10) 
-        const limit = parseInt(req.query.limit,10)
-        const results = {} 
-       
-        const totalDocslength = await StaticData.countDocuments()
-        const totalPages =  Math.ceil( totalDocslength/limit) 
-        results.totalPages = totalPages  
-         results.currentPage = page 
-        const startIndex = (page-1)*limit
-        const endIndex = page*limit
-        
-        if(startIndex > 0 ){
-            results.prevPage = page-1
-        }
-        if(endIndex < await StaticData.countDocuments() ){
-         results.nextPage = page+1
-        }
-     
-        const paginatedData = await StaticData.find().limit(limit).skip(startIndex)
-        results.paginatedData = paginatedData
-        res.Variableassign = results
-        next()
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+    //  const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    //  cb(null, file.fieldname + '-' + uniqueSuffix)
+    //same file repeat karnai kai liy
+     //  cb(null,file.originalname)
+     cb(null,Math.floor(Math.random()*100000) + file.originalname) 
     }
-   
-}
-
-
-app.get("/Users",paginationFunctionality(User),(req,res)=>{
-          res.json(res.Variableassign)
+  })
+  
+  const upload = multer({ storage: storage })
+app.post('/upoading',upload.single("file1"),(req,res)=>{
+    console.log(req.file) 
+    res.send("hello")
 })
 
-app.get("/getPaginationRender",(req,res)=>{
-   res.render('Index')
+app.listen(process.env.port,()=>{
+    console.log("api listen on port "+ process.env.port)
 })
 
 
