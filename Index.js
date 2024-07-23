0500308a013a7c6f3166989c4cca1d6d1341bc3a
sharmasuper <mohit6375sharma@gmail.com>
2024-07-23 13:33:52 +0530

    connect-image-optimus means in hindi

--- Index.js
+++ Index.js
@@ -1,55 +1,21 @@
-// cls-rtracer is a middleware for Express.js that provides request tracing, making it easier to track
-//  and log requests across the entire application, including asynchronous operations. 
-// It uses Continuation-Local Storage (CLS) to maintain context for the lifetime of a request.
-
-// const express = require('express');
-// const rTracer = require('cls-rtracer');
-
-// const app = express();
-
-// // Apply the rTracer middleware
-// app.use(rTracer.expressMiddleware());
-
-// app.get('/', (req, res) => {
-//   // Retrieve the request identifier
-//   const requestId = rTracer.id();
-//   res.send(`Request ID: ${requestId}`);
-// });
-
-// app.listen(3000, () => {
-//   console.log('Server is running on port 3000');
-// });
+// connect-image-optimus is a middleware for Express.js that optimizes images on the fly. It can be used to 
+// reduce the size of images and improve 
+// the performance of your web application by compressing images before they are sent to the client.
 
 const express = require('express');
-const rTracer = require('cls-rtracer');
-const winston = require('winston');
-
+const optimus = require('connect-image-optimus');
+const path = require('path')
 const app = express();
 
-// Create a winston logger
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message }) => {
-      const requestId = rTracer.id();
-      return `${timestamp} [${level}] [${requestId}] ${message}`;
-    })
-  ),
-  transports: [
-    new winston.transports.Console()
-  ]
-});
+// Apply the connect-image-optimus middleware
+app.use(optimus());
 
-// Apply the rTracer middleware
-app.use(rTracer.expressMiddleware());
+// Serve static files
+app.use(express.static('public/images/'));
 
-app.get('/', (req, res) => {
-  // Log a message with the request ID
-  logger.info('Handling request');
-  res.send('Hello, World!');
-});
 
 app.listen(3000, () => {
-  logger.info('Server is running on port 3000');
+  console.log('Server is running on port 3000');
 });
+
+// public/images/image-3d-ganesha-dark-background-diwali.jpg
